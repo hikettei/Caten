eb10ee04d7ad93507853c9f4cb83096519a74a9e
hikettei <hikettei@gmail.com>
2024-07-22 19:23:13 +0900

    enhancement: purge the node only after confirmed not anymore used, implemented constant folding

--- source/api/constant-folding.lisp
+++ source/api/constant-folding.lisp
@@ -8,12 +8,12 @@
 
 (defsimplifier
     (%1_fold_constant :speed 0)
-    ((:Add ((:_TmpScalarConst (x)) (:_TmpScalarConst (y))))
+    ((:Add ((:_TmpScalarConst (x) :dtype dtype) (:_TmpScalarConst (y))))
      ->
-     (:Const ((+ x y))))
-    ((:Mul ((:_TmpScalarConst (x)) (:_TmpScalarConst (y))))
+     (:_TmpScalarConst ((+ x y)) :dtype dtype))
+    ((:Mul ((:_TmpScalarConst (x) :dtype dtype) (:_TmpScalarConst (y))))
      ->
-     (:Const ((* x y)))))
+     (:_TmpScalarConst ((* x y)) :dtype dtype)))
 
 (defsimplifier
     (%2_unfold_load_alloc)
@@ -22,11 +22,16 @@
      ((node graph)
        (with-context-nodes (_ (%load (%salloc :dtype dtype) x :id (node->id node)))))))
 
-;; wip: 1 is pruned
 (defun fold-constant (graph)
+  (declare (type Graph graph))
+  (assert (null (find :_TmpScalarConst (graph-nodes graph) :key #'node-type))
+	  ()
+	  "_TmpScalarConst shouldn't exist!")
   (%0_fuse_load_alloc graph)
   (%1_fold_constant graph)
   (%2_unfold_load_alloc graph)
-
-  ;; TODO: Assert _TmpScalarConst is removed.
+  (assert (null (find :_TmpScalarConst (graph-nodes graph) :key #'node-type))
+	  ()
+	  "_TmpScalarConst shouldn't exist! (but it is a simplifier's bug)")
+  ;; TODO: Read undefined check
   graph)
