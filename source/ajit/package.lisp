44af1504bb59425eb40095463fce6e42e0ce530f
hikettei <hikettei@gmail.com>
2024-07-31 01:37:52 +0900

    Implemented type-inference

--- source/ajit/package.lisp
+++ source/ajit/package.lisp
@@ -1,6 +1,8 @@
 (in-package :cl-user)
 (defpackage :caten/ajit
-  (:use :cl :caten/aasm :caten/air :caten/avm))
+  (:use :cl :caten/aasm :caten/air :caten/avm)
+  (:export
+   ))
 (in-package :caten/ajit)
 
 
--- source/ajit/type-relay.lisp
+++ source/ajit/type-relay.lisp
@@ -1,52 +1,61 @@
 (in-package :caten/ajit)
 
+(defparameter *type-reporter* nil)
+(defstruct (Type-Reporter
+	    (:conc-name rp-)
+	    (:constructor make-type-reporter ()))
+  (id2buffer (make-hash-table :test #'eql)))
+
 (defstruct (FakeArray
 	    (:constructor make-fakearray (shape dtype initial-element)))
   (shape shape :type list)
   (dtype dtype :type dtype-t)
   (initial-element initial-element))
 
+(defun gid (n) (intern (format nil "_gid~a" n)))
 (defmethod %vm/allocate-buffer ((device-id (eql :relay-checker)) buffer)
   (let ((initial-value (if (eql (buffer-dtype buffer) :bool)
 			   nil
 			   (coerce 0 (dtype->lisp (buffer-dtype buffer))))))
     (if (= (buffer-nrank buffer) 0)
 	(setf (buffer-value buffer) (make-fakearray nil (buffer-dtype buffer) initial-value))
-	(setf (buffer-value buffer) (make-fakearray (buffer-shape buffer) (buffer-dtype buffer) initial-value)))))
+	(setf (buffer-value buffer) (make-fakearray (buffer-shape buffer) (buffer-dtype buffer) initial-value)))
+    buffer))
 
 (defmethod %impl :around ((device-id (eql :relay-checker)) op graph node args)
-  ;; TODO: Record outputs to hash-table
-  ;; NODE_ID <-> FakeAray corresponds one-by-one
-  ;; using such that information, we can schedule the graph
-  )
-
+  (let ((out-buffer (multiple-value-list (if (next-method-p) (call-next-method) (car args)))))
+    (when *type-reporter*
+      (loop for n in (node-writes node)
+	    for o in out-buffer
+	    do (setf (gethash n (rp-id2buffer *type-reporter*)) o)))
+    (apply #'values out-buffer)))
+(defmethod %impl ((device-id (eql :relay-checker)) op graph node args) (if (next-method-p) (call-next-method) (car args)))
 (defmethod %impl ((device-id (eql :relay-checker)) (op (eql :Allocate)) graph node args)
   (multiple-value-bind (shape stride) (parse-allocate-node node args)
     (realize-buffer graph (node->id node) :shape1 shape :stride1 stride)))
-
+(defmethod %impl ((device-id (eql :relay-checker)) (op (eql :view)) graph node args)
+  (multiple-value-bind (shape v1 v2 v3 stride bc)
+      (parse-view-node node args)
+    (flet ((->number (x) (if (buffer-p x) (buffer-value x) x)))
+      (let ((buffer (copy-buffer (car args))))
+	(setf (buffer-shape buffer) (map 'list #'->number shape)
+	      (buffer-stride buffer) (map 'list #'->number stride)
+	      (buffer-views buffer)
+	      (loop for i upfrom 0 below (length v1)
+		    collect (list (->number (nth i v1)) (->number (nth i v2)) (->number (nth i v3)) (nth i bc)))
+	      (buffer-nrank buffer) (length shape))
+	buffer))))
 (defmethod %impl ((device-id (eql :relay-checker)) (op (eql :Load)) graph node args)
   (let* ((tgt (car args))
 	 (val (getattr node :value)))
-    (if (= (buffer-nrank (car args)) 0)
-	(let ((out (copy-buffer tgt)))
-	  (setf (buffer-value out) val)
-	  out)
-	;; [TODO] map-view
-	)))
+    (let ((out (copy-buffer tgt)))
+      (setf (buffer-value out) val)
+      out)))
+(defmethod %impl ((device-id (eql :relay-checker)) (op (eql :WHERE)) graph node args) (second args))
 
+(defun type-infer (avm)
+  (declare (type avm avm))
+  (let ((*device* :relay-checker) (*type-reporter* (make-type-reporter)))
+    (vm/forward avm) (vm/backward avm)
+    *type-reporter*))
 
-(defmethod %impl ((device-id (eql :relay-checker)) (op (eql :store)) graph node args)
-  
-  )
-
-(defmethod %impl ((device-id (eql :relay-checker)) (op (eql :Index-Components)) graph node args)
-
-  )
-
-(macrolet ((impl (kw)  `(defmethod %impl ((device-id (eql :relay-checker)) (op (eql ,kw)) graph node args) (car args))))
-  (impl :add)  
-  )
-
-(defmethod %impl ((device-id (eql :relay-checker)) (op (eql :view)) graph node args)
-
-  )
