b4c217a65f0b6f91b1af05137c48ac21ef1fa1d1
hikettei <hikettei@gmail.com>
2024-08-10 15:17:42 +0900

    clean up renderer

--- source/ajit/backends/clang.lisp
+++ source/ajit/backends/clang.lisp
@@ -203,6 +203,19 @@ Compiled with: ~a"
     (:uint8 :uint8)
     (:int8 :int8)))
 
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun impl-unary (op render)
+    `(,op
+      (multiple-value-bind (a at) (values (car (node-reads node)) (car (relay-reads type)))
+	(multiple-value-bind (b bt) (values (car (node-writes node)) (car (relay-writes type)))
+	  (line "~(~a~) = ~a(~(~a~));" (render-aref b bt) ,render (render-aref a at))))))
+  (defun impl-binary (op render)
+    `(,op
+      (multiple-value-bind (c ct) (values (car (node-writes node)) (car (relay-writes type)))
+	(multiple-value-bind (a at) (values (car (node-reads node)) (car (relay-reads type)))
+	  (multiple-value-bind (b bt) (values (second (node-reads node)) (second (relay-reads type)))
+	    (line "~(~a~) = ~(~a~) ~a ~(~a~);" (render-aref c ct) (render-aref a at) ,render (render-aref b bt))))))))
+
 (defmethod %render-nodes ((lang (eql :clang)) graph access indent)
   (with-output-to-string (out)
     (macrolet ((line (designator &rest args)
@@ -242,53 +255,34 @@ Compiled with: ~a"
 		     (multiple-value-bind (ct at bt) (apply #'values (relay-reads type))
 		       (line "~(~a~) += ~(~a~) * ~(~a~);" (render-aref c ct) (render-aref a at) (render-aref b bt)))))
 		  (:EXPR
-		   (multiple-value-bind (ot) (apply #'values (relay-reads type))		     
-		       (line "~(~a~) = ~(~a~);" (render-aref (car (node-reads node)) ot) (render-expr lang (getattr node :EXPR)))))
+		   (multiple-value-bind (at) (apply #'values (relay-writes type))		     
+		     (line "~(~a~) = ~(~a~);" (render-aref (car (node-writes node)) at) (render-expr lang (getattr node :EXPR)))))
 		  (:STORE
 		   (multiple-value-bind (a b) (apply #'values (node-reads node))
 		     (multiple-value-bind (at bt) (apply #'values (relay-reads type))
 		       (when (not (equal a b))
 			 (line "~(~a~) = ~(~a~);" (render-aref a at) (render-aref b bt))))))
-		  (:SIN
-		   (multiple-value-bind (a at) (values (car (node-reads node)) (car (relay-reads type)))
-		     (line "~(~a~) = sin(~(~a~));" (render-aref a at) (render-aref a at))))
-		  (:LOG2
-		   (multiple-value-bind (a at) (values (car (node-reads node)) (car (relay-reads type)))
-		     (line "~(~a~) = log2(~(~a~));" (render-aref a at) (render-aref a at))))
-		  (:EXP2
-		   (multiple-value-bind (a at) (values (car (node-reads node)) (car (relay-reads type)))
-		     (line "~(~a~) = exp2(~(~a~));" (render-aref a at) (render-aref a at))))
-		  (:NEG
-		   (multiple-value-bind (a at) (values (car (node-reads node)) (car (relay-reads type)))
-		     (line "~(~a~) = -~(~a~);" (render-aref a at) (render-aref a at))))
-		  (:RECIP
-		   (multiple-value-bind (a at) (values (car (node-reads node)) (car (relay-reads type)))
-		     (line "~(~a~) = 1.0 / ~(~a~);" (render-aref a at) (render-aref a at))))
-		  (:SQRT
-		   (multiple-value-bind (a at) (values (car (node-reads node)) (car (relay-reads type)))
-		     (line "~(~a~) = sqrt~(~a~);" (render-aref a at) (render-aref a at))))
-		  (:NOT
-		   (multiple-value-bind (a at) (values (car (node-reads node)) (car (relay-reads type)))
-		     (line "~(~a~) = !~(~a~);" (render-aref a at) (render-aref a at))))
+		  (:MOVE
+		   (multiple-value-bind (a b) (apply #'values (node-reads node))
+		     (multiple-value-bind (at bt) (apply #'values (relay-reads type))
+		       (line "~(~a~) = ~(~a~);" (render-aref a at) (render-aref b bt)))))
+		  #.(impl-unary :SIN "sin")
+		  #.(impl-unary :LOG2 "log2")
+		  #.(impl-unary :EXP2 "exp2")
+		  #.(impl-unary :NEG "-")
+		  #.(impl-unary :RECIP "1.0 / ")
+		  #.(impl-unary :SQRT "sqrt")
+		  #.(impl-unary :NOT "!")
 		  (:INDEX-COMPONENTS
 		   (line "~(~a~) = ~(~a~);" (render-aref (car (node-reads node)) (car (relay-reads type))) (render-isl-aref (car (relay-reads type)) :genid #'(lambda (x) (intern (format nil "c~a" x))))))
-		  (:MOVE
-		   (multiple-value-bind (a at) (values (car (node-reads node)) (car (relay-reads type)))
-		     (multiple-value-bind (b bt) (values (second (node-reads node)) (second (relay-reads type)))
-		       (line "~(~a~) = ~(~a~);" (render-aref a at) (render-aref b bt)))))
-		  (:OR
-		   (multiple-value-bind (a at) (values (car (node-reads node)) (car (relay-reads type)))
-		     (multiple-value-bind (b bt) (values (second (node-reads node)) (second (relay-reads type)))
-		       (line "~(~a~) = ~(~a~) | ~(~a~);" (render-aref a at) (render-aref a at) (render-aref b bt)))))
-		  (:AND
-		   (multiple-value-bind (a at) (values (car (node-reads node)) (car (relay-reads type)))
-		     (multiple-value-bind (b bt) (values (second (node-reads node)) (second (relay-reads type)))
-		       (line "~(~a~) = ~(~a~) & ~(~a~);" (render-aref a at) (render-aref a at) (render-aref b bt)))))
-		  (:WHERE		   
-		   (multiple-value-bind (a at) (values (nth 0 (node-reads node)) (nth 0 (relay-reads type)))
-		     (multiple-value-bind (b bt) (values (nth 1 (node-reads node)) (nth 1 (relay-reads type)))
-		       (multiple-value-bind (c ct) (values (nth 2 (node-reads node)) (nth 2 (relay-reads type)))
-			 (line "~(~a~) = ~(~a~) ? ~(~a~) : ~(~a~);" (render-aref b bt) (render-aref a at) (render-aref b bt) (render-aref c ct))))))
+		  #.(impl-binary :OR "|")
+		  #.(impl-binary :AND "&")
+		  (:WHERE
+		   (multiple-value-bind (o ot) (values (car (node-writes node)) (car (relay-writes type)))
+		     (multiple-value-bind (a at) (values (nth 0 (node-reads node)) (nth 0 (relay-reads type)))
+		       (multiple-value-bind (b bt) (values (nth 1 (node-reads node)) (nth 1 (relay-reads type)))
+			 (multiple-value-bind (c ct) (values (nth 2 (node-reads node)) (nth 2 (relay-reads type)))
+			   (line "~(~a~) = ~(~a~) ? ~(~a~) : ~(~a~);" (render-aref o ot) (render-aref a at) (render-aref b bt) (render-aref c ct)))))))
 		  (otherwise
 		   (case (node-class node)
 		     (:BinaryOps
@@ -298,7 +292,7 @@ Compiled with: ~a"
 			(if op
 			    (if r
 				(line "~(~a~) ~a ~(~a~);" (render-aref (car (node-reads node)) (car (relay-reads type))) op (render-aref (second (node-reads node)) (second (relay-reads type))))
-				(line "~(~a~) = ~(~a~);" (render-aref (car (node-reads node)) (car (relay-reads type)))
+				(line "~(~a~) = ~(~a~);" (render-aref (car (node-writes node)) (car (relay-reads type)))
 				      (apply
 				       #'concatenate
 				       'string
@@ -308,7 +302,7 @@ Compiled with: ~a"
 					      append (list (render-aref r rt) op))))))
 			    (let ((op (ecase (node-type node)
 					(:MAX "max"))))
-			      (line "~(~a~) = ~a(~a);" (render-aref (car (node-reads node)) (car (relay-reads type))) op
+			      (line "~(~a~) = ~a(~a);" (render-aref (car (node-writes node)) (car (relay-writes type))) op
 				    (apply
 				     #'concatenate
 				     'string
@@ -318,9 +312,9 @@ Compiled with: ~a"
 					    append (list (render-aref r rt) ", ")))))))))
 		     (:TernaryOps
 		      (let ((op (ecase (node-type node) (:NEQ "!=") (:LT "<"))))
-			(multiple-value-bind (a at) (values (nth 0 (node-reads node)) (nth 0 (relay-reads type)))
+			(multiple-value-bind (o ot) (values (car (node-writes node)) (car (relay-writes type)))
 			  (multiple-value-bind (b bt) (values (nth 1 (node-reads node)) (nth 1 (relay-reads type)))
 			    (multiple-value-bind (c ct) (values (nth 2 (node-reads node)) (nth 2 (relay-reads type)))
-			      (line "~(~a~) = ~(~a~) ~a ~(~a~);" (render-aref a at) (render-aref b bt) op (render-aref c ct)))))))
+			      (line "~(~a~) = ~(~a~) ~a ~(~a~);" (render-aref o ot) (render-aref b bt) op (render-aref c ct)))))))
 		     (otherwise
 		      (error "Renderer for ~a is not implemented yet." node))))))))))
