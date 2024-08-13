41722990e620604bd3bc705c7e146c493055c166
hikettei <hikettei@gmail.com>
2024-08-13 12:56:54 +0900

    control-flow was born

--- source/apis/control-flow.lisp
+++ source/apis/control-flow.lisp
@@ -1,16 +1,1 @@
 (in-package :caten/apis)
-
-(defparameter *conditioned* nil)
-(defmacro !if (condition then &optional else))
-
-(!if (> n 1)
-     nil
-     )
-
-(defmacro !when (condition &rest forms) `(!if ,condition (progn ,@forms)))
-
-(defparameter *iterate-over* nil)
-;; TODO: Implement RNN
-;; TODO: Implement KV Cache
-(defmacro !loop (&rest keyword-and-forms)
-  )
