486807f338dcd774914477f58f85490a939d8ab4
hikettei <hikettei@gmail.com>
2024-08-15 11:12:56 +0900

    typog

--- source/avm/opset.lisp
+++ source/avm/opset.lisp
@@ -5,8 +5,8 @@
 (defmethod %impl :around (device-id (op (eql :Allocate)) graph node args)
   (let ((from (getattr node :from)))
     ;; See ajit/type-relay.lisp: we need to feed a fake-array to relay-checker instead of the realized buffer.
-    (if (or from (not (eql device-id :relay-checker)))
+    (if (or (null from) (eql device-id :relay-checker))
+	(call-next-method)
 	(progn
 	  (assert (buffer-p from) () ":from attribute for ~a must be a realized buffer! (check the graph construction process.)" node)
-	  from)
-	(call-next-method))))
+	  from))))
