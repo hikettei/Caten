815ce23932689e21065fe010e84e57a4e322e7dd
hikettei <hikettei@gmail.com>
2024-08-23 11:45:08 +0900

    optimize assign

--- source/aasm/optimizers.lisp
+++ source/aasm/optimizers.lisp
@@ -8,7 +8,7 @@
       (when (and (eql dtype1 dtype2) (equal s1 s2))
 	(make-node :Buffer :Allocate (node-writes node) s1 :nrank nrank :dtype dtype1)))))
 
-(defun remove-duplicated-load (graph)
+(defun rewrite-duplicated-load (graph)
   "
 Variables are allocated once.
 tmp1 <- A
@@ -17,23 +17,40 @@ tmp2 <- A
 tmp1 <- A
 tmp2 <- tmp1"
   (declare (type graph graph))
-  (let ((variables (make-hash-table :test #'eql)))
+  (let ((variables (make-hash-table :test #'eql))
+	(assigns))
     (flet ((getvar (id) (gethash id variables))
 	   (setvar (id val) (setf (gethash id variables) val)))
       (setf (graph-nodes graph)
 	    (loop for node in (graph-nodes graph)
 		  for type = (node-type node)
-		  if (eql type :Load)
+		  if (and
+		      (eql type :Load)
+		      (let ((prev (id->value graph (car (node-reads node)))))
+			(eql (node-type prev) :Allocate)
+			))
 		    collect
 		    (if (getvar (getattr node :value))
-			(progn
-			  
-			  )
+			(let ((assign (make-node :Buffer :ASSIGN (list (car (node-writes node))) (list (getvar (getattr node :value))))))
+			  (push assign assigns)
+			  assign)
 			(progn
 			  (setvar (getattr node :value) (car (node-writes node)))
-			  node)))))))
+			  node))
+		  else
+		    collect node))
+      (loop for assign in (nreverse assigns)
+	    for key = (car (node-writes assign))
+	    for to = (car (node-reads assign))
+	    if (null (find to (graph-outputs graph))) do
+	      (flet ((new (id) (if (eql id key) to id)))
+		(dolist (node (graph-nodes graph))
+		  (setf (node-reads node) (map 'list #'new (node-reads node))))
+		(remnode graph (node-id assign)))))))
 
 (defun optimize-aasm (graph)
   (declare (type graph graph))
   (fold-constant graph)
-  (fuse-duplicated-move graph))
+  (fuse-duplicated-move graph)
+  (rewrite-duplicated-load graph)
+  (verify-graph graph))
