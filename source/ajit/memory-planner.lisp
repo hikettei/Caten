a932379f8904ecebc4f500f55eff0933487dfea9
hikettei <hikettei@gmail.com>
2024-08-25 20:47:16 +0900

    more detail

--- source/ajit/memory-planner.lisp
+++ source/ajit/memory-planner.lisp
@@ -313,9 +313,10 @@ Refcount-by:
 		    (push s meta-ids)))))
 	    (setf (kernel-renderer-args kernel) (remove-duplicates kernel-args :key #'argument-name))
 	    (assert (equal (map 'list #'argument-name (kernel-renderer-args kernel))
-			   (map 'list (compose #'newid #'argument-name) (kernel-renderer-args kernel))))))
+			   (map 'list (compose #'newid #'argument-name) (kernel-renderer-args kernel)))
+		    ()
+		    "There are inconsistencies in the time-series dependencies. ~a" (map 'list #'argument-name (kernel-renderer-args kernel)))))
       (remove-unused-kernels! kernels pipeline save-for-backwards meta-ids)
-      ;; ここでArgsの判定 etc
       ;; Reduction
       ;;   Reduce  [1, 2, 3] -> [1]
       ;;   Scatter [1] -> [1 2 3]
