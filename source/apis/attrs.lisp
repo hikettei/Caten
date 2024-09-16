dff7e2296edb2b37ef3c00d6b68ec252288d44f7
hikettei <hikettei@gmail.com>
2024-09-16 23:55:52 +0900

    JITAble

--- source/apis/attrs.lisp
+++ source/apis/attrs.lisp
@@ -1,3 +1,3 @@
 (in-package :caten/apis)
 
-(defnode (:Special/VM :Pause/Backward) () "During VM execution, the forward computation is paused at the point where this node exists.")
+(defnode (:Special/VM :Pause/Backward) (JITAble) "During VM execution, the forward computation is paused at the point where this node exists.")
