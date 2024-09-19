eeebf24f5da647265b993129fc6d356114c75815
hikettei <hikettei@gmail.com>
2024-09-19 20:10:24 +0900

    Py4CL-Approach

--- source/test-suite/package.lisp
+++ source/test-suite/package.lisp
@@ -2,6 +2,6 @@
 
 (defpackage :caten/test-suite
   (:use :cl :rove :caten :caten/nn :caten/air :caten/aasm :caten/avm :caten/common.dtype :alexandria
-	:caten/llm))
+	:caten/llm :py4cl))
 
 (in-package :caten/test-suite)
