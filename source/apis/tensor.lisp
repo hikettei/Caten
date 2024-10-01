367e250c3884c6a8f8ef2507f47027f94dcb68af
hikettei <hikettei@gmail.com>
2024-10-01 14:57:42 +0900

    WIP: Caten/APIs Tensor Documentation

--- source/apis/tensor.lisp
+++ source/apis/tensor.lisp
@@ -7,7 +7,22 @@
 						   (variables nil) (views nil) (requires-grad nil)
 						   (grad (when requires-grad (make-tensor shape :dtype dtype :order order :requires-grad nil :id (gensym "GRAD"))))
 						   (grad-id (when requires-grad (gensym "TGRAD"))))))
-  "Tensor is a lazy tensor object. It is a core data structure of the Caten system."
+  "
+A struct `tensor` is a multi-dimensional, and strided matrix (and nrank=0 to scalar value) containing elements of the single dtype.
+Also the tensor has following slots:
+
+- shape[list] A list of number, symbol, or `Tensor`.
+- buffer[Buffer] A buffer of the tensor. Realized arrays are stored here.
+- dtype[keyword] A dtype of the tensor.
+- order[order] A memory layout of the tensor, selected from either of :row or :column.
+- id[symbol] A unique identifier of the tensor. (usually created by gensym)
+- op[Func] `Func` object that represents the operation of the tensor.
+- views[list] A list of `ViewRange` objects, determining the bound of loops.
+- requires-grad[boolean] A flag to determine whether the tensor requires a gradient.
+- grad[Tensor] A gradient and realized tensor of the tensor.
+- grad-id[symbol] A unique identifier of the gradient tensor.
+- variables[list] A list of `Tensor` objects that are used in the operation of the tensor. Tensors listed here are involved in the compilation.
+"
   (shape shape :type list)
   (buffer nil :type (or null Buffer))
   (dtype dtype :type dtype-t)
@@ -32,11 +47,45 @@
      :views nil :requires-grad ,(tensor-requires-grad tensor)
      :grad ,(tensor-grad tensor) :grad-id ',(tensor-grad-id tensor))))
 
-(defun grad (tensor) "Returns a gradient of the tensor" (tensor-grad tensor))
-(defun shape (tensor) "Returns a copy of the shape of the tensor" (copy-list (tensor-shape tensor)))
-(defun ndim (tensor) "Returns a rank of the tensor" (length (shape tensor)))
-(defun dtype-of (tensor) "Returns a dtype of the tensor" (tensor-dtype tensor))
-(defun order (tensor) "Returns a memory-layout of the tensor." (tensor-order tensor))
+(defun grad (tensor)
+  "
+```
+(grad tensor)
+```
+Returns a gradient of the tensor"
+  (tensor-grad tensor))
+
+(defun shape (tensor)
+  "
+```
+(shape tensor)
+```
+Returns a copy of the shape of the tensor"
+  (copy-list (tensor-shape tensor)))
+
+(defun ndim (tensor)
+  "
+```
+(ndim tensor)
+```
+Returns a rank of the tensor"
+  (length (shape tensor)))
+
+(defun dtype-of (tensor)
+  "
+```
+(dtype-of tensor)
+```
+Returns a dtype of the tensor"
+  (tensor-dtype tensor))
+
+(defun order (tensor)
+  "
+```
+(order tensor)
+```
+Returns a memory-layout of the tensor."
+  (tensor-order tensor))
 
 (defmethod print-object ((tensor Tensor) stream)
   (format stream "{Tensor[~(~a~)] :shape ~a :id ~a
@@ -55,9 +104,21 @@
 	  (map 'list #'tensor-id (tensor-variables tensor))))
 
 (defun make-tensor (shape &key (dtype *default-float*) (order *default-order*) (id (gensym "TID")) (requires-grad nil) (initial-element nil) (views nil) (from nil))
-  "## [function] make-tensor
+  "
+```
+(make-tensor shape &key (dtype *default-float*) (order *default-order*) (id (gensym \"TID\")) (requires-grad nil) (initial-element nil) (views nil) (from nil))
+```
 Create a new lazy tensor.
-Shape := (Integer > 1) | Symbol | Tensor"
+
+- shape[list] A list of number, symbol, or `Tensor`.
+- dtype[keyword] A dtype of the tensor.
+- order[order] A memory layout of the tensor, selected from either of :row or :column.
+- id[symbol] A unique identifier of the tensor. (usually created by gensym)
+- requires-grad[boolean] A flag to determine whether the tensor requires a gradient.
+- initial-element[null|number|symbol|ScalarTensor] An initial value of the tensor.
+- views[list] A list of `ViewRange` objects, determining the bound of loops.
+- from[null|Buffer|Symbol] A buffer used to initialize the tensor. If symbol is given, the buffer is taken from the variable table.
+"
   (declare (type list shape)
 	   (type dtype-t dtype)
 	   (type (member :column :row) order)
@@ -73,7 +134,20 @@ Shape := (Integer > 1) | Symbol | Tensor"
     buff))
 
 (defun make-scalar (value &key (dtype *default-float*) (order *default-order*) (id (gensym "SID")) (requires-grad nil))
-  ""
+  "
+```
+(make-scalar value &key (dtype *default-float*) (order *default-order*) (id (gensym \"SID\")) (requires-grad nil))
+```
+Create a new scalar tensor. ScalarTensor in Caten is a tensor with a rank of 0.
+
+- value[number|symbol] A value of the scalar tensor.
+- dtype[keyword] A dtype of the tensor.
+- order[order] A memory layout of the tensor, selected from either of :row or :column.
+- id[symbol] A unique identifier of the tensor. (usually created by gensym)
+- requires-grad[boolean] A flag to determine whether the tensor requires a gradient.
+
+Tips: You can use `fconst`, `uconst`, and `iconst` to create a scalar tensor with a default dtype. (arguments are the same as `make-scalar`.)
+"
   (make-tensor nil :dtype dtype :order order :id id :requires-grad requires-grad :initial-element value))
 
 (macrolet ((def (name dtype)
@@ -86,6 +160,10 @@ Shape := (Integer > 1) | Symbol | Tensor"
   (def iconst *default-int*))
 
 (defun make-view-internal (base subscripts &key (allow-merge t) (dtype (tensor-dtype base)) (order (tensor-order base)) (id (gensym "VID")) (stride nil))
+  "
+An internal function for making a view from tensor.
+View is a tensor which shares the buffer from the original tensor, but having different shapes, strides, offsets, or dtype
+"
   (declare (type Tensor base)
 	   (type list subscripts)
 	   (type dtype-t dtype)
