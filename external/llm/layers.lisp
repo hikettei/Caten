727d0ceb2039c0657b0bb88d86067403a954f262
hikettei <hikettei@gmail.com>
2024-10-02 17:45:23 +0900

    Enhancement: Masked the input of the MHA

--- external/llm/layers.lisp
+++ external/llm/layers.lisp
@@ -86,12 +86,17 @@
 (defmethod call ((model Transformer) &rest inputs)
   (multiple-value-bind (tokens start-pos temperature) (apply #'values inputs)
     (assert (and tokens start-pos))
+    (st "Tokens[batch sentence_length] Start_Pos[] Tmp[] -> Tokens[batch sentence_length]" (tokens start-pos temperature))
     (with-slots ((wte wte) (wpe wpe) (h h) (lm-head lm-head)) model
       (let* ((token-emb (forward wte tokens))
 	     (pos-emb (forward wpe (!cast (!add start-pos (!index-components `(1 ,(second (shape tokens))))) (dtype-of tokens))))
 	     (hi (!add token-emb pos-emb))
-	     ;; TODO: triu
-	     (mask nil)
+             (seq-len (iconst (second (shape tokens))))
+	     (mask (!triu
+                    (!full
+                     `(1 1 ,seq-len ,(!add start-pos seq-len))
+                     (-inf))
+                    :diagonal (!+ (iconst 1) start-pos)))
 	     (_ (loop for hn in h do
 	       (setf hi (forward hn hi start-pos mask))))
 	     (logits (forward lm-head hi)))
