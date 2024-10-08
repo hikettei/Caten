515d2fb845c1b450f5c4630556b6244ef152623b
hikettei <hikettei@gmail.com>
2024-10-08 17:34:00 +0900

    Test: ignore 0

--- source/ajit/isl-objects.lisp
+++ source/ajit/isl-objects.lisp
@@ -74,7 +74,6 @@
   (and
    (not (eql (node-class node) :IR))
    (not (eql (node-type node) :Allocate))))
-
 ;; ~~ AREF ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 (defun isl-access-expr-no-stride (gid stride upfrom by broadcast-p)
   (declare (ignore stride))
@@ -129,7 +128,8 @@ A[stride1 * view_info1 * index_component_0 + bias1 + stride2 * view_info2 * inde
 	 #'concatenate 'string
 	 (butlast
           (loop for idx in (nconc indices (when upper (loop repeat (- upper c) collect (make-expr 0 nil))))
-		append (list (render-expr (default-device :clang) idx) ", "))))
+                if (not (expr-eq idx (make-const 0 nil)))
+		  append (list (render-expr (default-device :clang) idx) ", "))))
 	(flet ((add (x y) (make-expr :ADD x y)))
 	  (if (null indices)
 	      nil
