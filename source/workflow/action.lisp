cc37b41d248c72da3c6be3b43e7696886212cc94
hikettei <hikettei@gmail.com>
2024-10-11 15:05:51 +0900

    .

--- source/workflow/action.lisp
+++ source/workflow/action.lisp
@@ -4,7 +4,6 @@
 ;; Goal: Export the entire code (including tokenizer, data loader, matrix computation kernel) for any language
 ;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-
 ;; caten/workflow
 ;; - Includes a wrapper for `caten`.
 ;; - Includes a class for Tokenizer.
@@ -15,13 +14,21 @@
 ;; - Implement Control, High-Level Interface, Including Function defining, IF/FOR
 ;;   - DSL
 
+;; [TODO]
+;; - ここでこのモデル全体をCommon Lispで読み込むことによって，モデルのコンパイル結果をCache可能にする
+;; - 若しくは，C言語/Python/Smaller Common LispへCompileして実行を可能に
+
+;; Backend has two facets:
+;; - Kernel Renderer   [ Low-Level  ]
+;; - Workflow Renderer [ High-Level ]
+
 ;; TODO: Compile the entire graph. Including token sampling
 ;;      Input
 ;;        |
 ;; [Action: Tokenizer]
 ;;        |
 ;;  |-[Action:Run]-|
-;;  |  Transformer | <-------------------------------|
+;;  | Transformer  | <-------------------------------|
 ;;  |--------------|                                 |
 ;;        |                                          | x N
 ;;        |----[Action: Logits Argmax Concatenate] --|
@@ -29,3 +36,16 @@
 ;;    [Output]
 ;;        |
 
+
+(defmacro defaction (name &body body)
+  
+  )
+(defmacro defworkflow (name &body body)
+
+  )
+
+;; (defaction switch (condition action1 action2)
+
+(defworkflow TransformerInference
+  A -> B -> (IF X -> C)
+  )
