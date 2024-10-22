cef7bd9fb9a9aef4abd5aed8334d44b6eb4d6fe6
hikettei <hikettei@gmail.com>
2024-10-22 22:38:46 +0900

    expr-cache

--- source/codegen/expr-cache.lisp
+++ source/codegen/expr-cache.lisp
@@ -1,24 +1,55 @@
 (defpackage :caten/codegen/expr-cache
+  (:documentation "stash-expr to make the same expr to be the same named symbol. restore-expr to retrive the original expr.")
   (:use :cl :caten/codegen/expr :caten/codegen/renderer)
   (:export
    #:Expr-Cache
    #:with-expr-cache
-   ))
+   #:stash-expr
+   #:restore-expr))
 
 (in-package :caten/codegen/expr-cache)
-
+;; [TODO] Use this to optimize expr-scalar-equivalent-p
 (defvar *expr-cache*)
 
 (defclass Expr-Cache ()
   ((cache :type hash-table :initform (make-hash-table :test 'equal) :accessor cache-table)
-   (global-counter :initform 0 :type fixnum :accessor expr-cache)))
+   (id2expr :type hash-table :initform (make-hash-table :test 'equal) :accessor id2expr-table)
+   (global-counter :initform 0 :type fixnum :accessor global-counter))
+  (:documentation "Creates a cached object for (scalar) EXPR graph."))
 
 (defmacro with-expr-cache (() &body body)
   `(let ((*expr-cache* (make-instance 'Expr-Cache)))
      ,@body))
 
 (defun expr-id (num) (format nil "_expr_id_~a" num))
+(defun expr-hash-key (expr) (render-expr 'Default-Renderer expr))
 
-(defmethod stash-expr ((expr-cache Expr-Cache) (expr Expr))
+(defmethod %stash-expr ((expr-cache Expr-Cache) (expr Expr))
+  (let ((key (expr-hash-key expr)))
+    (if (gethash key (cache-table expr-cache))
+        (values (expr-id (gethash key (cache-table expr-cache))) nil)
+        (progn
+          (incf (global-counter expr-cache))
+          (setf (gethash key (cache-table expr-cache)) (global-counter expr-cache)
+                (gethash key (id2expr-table expr-cache)) expr)
+          (values (expr-id (global-counter expr-cache)) t)))))
 
-  )
+(declaim (ftype (function (Expr) (values string boolean &optional)) stash-expr))
+(defun stash-expr (expr)
+  "Return: (values expr_id is_new)"
+  (declare (type Expr expr))
+  (assert *expr-cache* () "*expr-cache* is not set.")
+  (%stash-expr *expr-cache* expr))
+
+(declaim (ftype (function (string) (or Expr null)) restore-expr))
+(defun restore-expr (expr-id)
+  (declare (type string expr-id))
+  (assert *expr-cache* () "*expr-cache* is not set.")
+  (let ((key (format nil "~(~a~)" expr-id)))
+    (when (or (<= (length key) 9)
+              (not (string= "_expr_id_" (subseq key 0 9))))
+      (return-from restore-expr nil))
+    (or
+     (gethash key (id2expr-table *expr-cache*))
+     (warn "Undefined EXPR ID ~a" key)
+     nil)))
