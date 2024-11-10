46a5a06e18f361060a4fa1c7cd1075720dafd26f
hikettei <hikettei@gmail.com>
2024-11-10 21:42:12 +0900

    Test suites for Avg/Max Pooling

--- source/test-suite/test-pool.lisp
+++ source/test-suite/test-pool.lisp
@@ -1,5 +1,15 @@
 (in-package :caten/test-suite)
 
+(deftest test-avg-pooling
+  (with-given-dtype ((:float32 . "float32"))
+    (with-no-grad
+      (let ((input (rand `(10 3 25 25))))
+        (assert-equal
+            (:atol 1e-5 :rtol 1e-5)
+            (with-torch (input)
+              (->caten (f:avg_pool2d input `(2 2))))
+            (proceed (!AvgPool2D input :kernel-size `(2 2))))))))
+
 (deftest test-max-pooling
   (with-given-dtype ((:float32 . "float32"))
     (with-no-grad
