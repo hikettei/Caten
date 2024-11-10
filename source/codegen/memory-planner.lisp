81da7590f4bbd92236603e615343af47d201d077
hikettei <hikettei@gmail.com>
2024-11-10 18:33:32 +0900

    reimplement memory planner

--- source/codegen/memory-planner.lisp
+++ source/codegen/memory-planner.lisp
@@ -83,7 +83,7 @@ MemoryBlock(id) is allocated when t=create, preserved until t become `release`."
         ;; If :from is specified => the input should not be destructed.
         t))))
 
-(defun rewrite-bp-with-newid (item newid no-rewrite)
+(defun rewrite-bp-with-newid (item newid)
   (dolist (bp (getattr item :blueprint))
     (setf (node-writes bp) (map 'list newid (node-writes bp))
           (node-reads bp) (map 'list newid (node-reads bp)))
@@ -100,125 +100,71 @@ MemoryBlock(id) is allocated when t=create, preserved until t become `release`."
                 collect item
               if (eql (node-type item) :DEFINE-GLOBAL)
                 do (push (car (node-writes item)) seen)))
-  (unless no-rewrite
-    (let* ((reads (map 'list #'cons (getattr item :storage-id-src) (getattr item :read-types)))
-           (writes (map 'list #'cons (getattr item :storage-id-dst) (getattr item :write-types)))
-           (reads (remove-duplicates reads :key (compose newid #'car)))
-           (writes (remove-duplicates writes :key (compose newid #'car)))
-           (seen))
-      (flet ((only-unseen (items)
-               (loop for (id . type) in items
-                     if (null (find (funcall newid id) seen))
-                       do (push (funcall newid id) seen) and collect (cons id type))))
-        (multiple-value-bind (writes reads) (values (only-unseen writes) (only-unseen reads))
-          (setf (getattr item :storage-id-src) (map 'list (compose newid #'car) reads)
-                (getattr item :storage-id-dst) (map 'list (compose newid #'car) writes)
-                (getattr item :read-types) (map 'list #'cdr reads)
-                (getattr item :write-types) (map 'list #'cdr writes)))))))
+  (let* ((reads (map 'list #'cons (getattr item :storage-id-src) (getattr item :read-types)))
+         (writes (map 'list #'cons (getattr item :storage-id-dst) (getattr item :write-types)))
+         (reads (remove-duplicates reads :key (compose newid #'car)))
+         (writes (remove-duplicates writes :key (compose newid #'car)))
+         (seen))
+    (flet ((only-unseen (items)
+             (loop for (id . type) in items
+                   if (null (find (funcall newid id) seen))
+                     do (push (funcall newid id) seen) and collect (cons id type))))
+      (multiple-value-bind (writes reads) (values (only-unseen writes) (only-unseen reads))
+        (setf (getattr item :storage-id-src) (map 'list (compose newid #'car) reads)
+              (getattr item :storage-id-dst) (map 'list (compose newid #'car) writes)
+              (getattr item :read-types) (map 'list #'cdr reads)
+              (getattr item :write-types) (map 'list #'cdr writes))))))
 
-(defmethod run-memory-planner-global ((schedule-graph Graph) (symbolics list) (base-graph Graph))
-  "write_1, write_2 = f(write_suite_1, write_suite_2, *[dynamic_shape + read_buffers])
-The goal of run-memory-planner is to reduce the number of :allocate-p object in schedule-graph, by rewriting write_suite1 and write_suite2."
-  (let* ((trace-table (make-hash-table))
-	 (id2type (make-hash-table))
-	 (lock-table (make-hash-table))
-	 (total-time (length (graph-nodes schedule-graph)))
-         (outputs (append (graph-outputs schedule-graph) symbolics)))
-    (dolist (o outputs) (setf (gethash o lock-table) t))
-    (loop for node in (graph-nodes schedule-graph)
-	  for nth upfrom 0
-          for lock-p = (null (getattr node :jitable)) do
-            (assert (= (length (getattr node :storage-id-src)) (length (getattr node :read-types))))
-            (assert (= (length (getattr node :storage-id-dst)) (length (getattr node :write-types))))
-	    (loop for val in (getattr node :storage-id-src)
-		  for typ in (getattr node :read-types)
-		  for time = `(,nth ,@(gethash val trace-table))
-                  if (id-is-input-p val base-graph) do (push val outputs)
-                  if (symbolp val)
-                    do (setf (gethash val id2type) typ (gethash val trace-table) time)) ;; (incf consume)
-	    (loop for val in (getattr node :storage-id-dst)
-		  for typ in (getattr node :write-types)
-                  if (id-is-input-p val base-graph) do (push val outputs)
-		  if (and (symbolp val) (null (gethash val trace-table)))
-                    ;; ID2Type    -> the variable name and its type
-                    ;; TraceTable -> the variable name and timestamps of the variable (when it's used)
-                    ;; LockTable  -> Set T to lock (never become in-place)
-		    do (setf (gethash val id2type) typ
-			     (gethash val trace-table) (list nth)
-                             (gethash val lock-table) lock-p)))
-    ;(maphash
-    ; #'(lambda (key val)
-    ;     (format t "~a -> ~a~%" key val))
-    ; trace-table)
-    (let* ((memory-blocks
-	     (loop for key in (alexandria:hash-table-keys trace-table)
-	           for typ = (gethash key id2type)
-		   collect
-                   ;; [Note] A memory block lives in the range of [min{t}, max{t})
-                   ;; Plus, If the same task (e.g.: T0(x) -> T1(x) -> T0(x+1)) is scheduled, the memory block lives from 0 to 2.
-		   (make-memoryblock
-		    key typ
-		    (apply #'min (gethash key trace-table))
-                    ;; Set the longest time for the output variables (not to destruct it, and users can see the result)
-		    (if (find key outputs)
-			total-time
-		        (apply #'max (gethash key trace-table))) ;; the fragment will be available after t+1.
-		    :lock (gethash key lock-table))))
-           ;; Minimize the peak memory usage
-	   (solved (greedy-solve-dsa memory-blocks total-time))
-           ;; Retrive the solution. A hash table of OLD_MEMORY_ID -> NEW_MEMORY_ID
-           (alias-map (make-hash-table)))
-      (loop for mb in solved
-            do (setf (gethash (memoryblock-id mb) alias-map) (or (memoryblock-answer mb) (memoryblock-id mb))))
-      ;(maphash
-      ; #'(lambda (key val)
-      ;     (format t "~a -> ~a~%" key val))
-      ; alias-map)
-      (labels ((newid (id)
-                 (if (gethash id alias-map)
-                     (if (eql (gethash id alias-map) id)
-                         id
-                         (newid (gethash id alias-map)))
-                     id)))
-        (dolist (node (graph-nodes schedule-graph))
-          (when (getattr node :jitable)
-            (rewrite-bp-with-newid node #'newid nil)))))))
-
-(defun run-memory-planner-local (item schedule-graph symbolics base-graph)
-  "Minimizes the number of allocation buffers that are only used in the item."
-  (declare (type node item) (type graph schedule-graph))
-  (assert (eql (node-type item) :Schedule-Item))
-  (let* ((blueprint (getattr item :blueprint))
+(defun apply-memory-planner (schedule-graph symbolics base-graph)
+  (declare (type graph schedule-graph))
+  (let* ((nodes
+           (loop for node in (graph-nodes schedule-graph)
+                 if (getattr node :jitable)
+                   append (getattr node :blueprint)
+                 else
+                   collect node))
+         (total-time (length nodes))
          (trace-table (make-hash-table))
 	 (id2type (make-hash-table))
 	 (lock-table (make-hash-table))
-	 (total-time (length blueprint))
          (outputs ;; a list of buffers that do no changed by the memory-planner
              (append ;; If the output were read by other kernels, it should be optimized by the global memory-planner.
               (graph-outputs schedule-graph)
-              symbolics
-              (loop for node in (graph-nodes schedule-graph)
-                    if (not (eql (node-id node) (node-id item)))
-                      append (node-reads node)))))
+              symbolics)))
     (dolist (o outputs) (setf (gethash o lock-table) t))
-    (loop for node in blueprint
+    (loop for node in nodes
 	  for nth upfrom 0
-          if (not (eql (node-class node) :Render)) do
-	    (loop for val in (node-reads node)
-		  for typ in (relay-reads (read-type-relay node))
-		  for time = `(,nth ,@(gethash val trace-table))
-                  if (id-is-input-p val base-graph) do (push val outputs)
+          if (eql (node-type node) :Schedule-Item) do
+            (loop for val in (getattr node :storage-id-src)
+                  for typ in (getattr node :read-types)
+                  for time = `(,nth ,@(gethash val trace-table))
+                  if (id-is-input-p val base-graph) do (setf (gethash val lock-table) t)
                     if (symbolp val)
-                      do (setf (gethash val id2type) typ (gethash val trace-table) time)) ;; (incf consume)
-	    (loop for val in (node-writes node)
-		  for typ in (relay-writes (read-type-relay node))
-                  if (id-is-input-p val base-graph) do (push val outputs)
-		    if (and (symbolp val) (null (gethash val trace-table)))
-                      ;; ID2Type    -> the variable name and its type
-                      ;; TraceTable -> the variable name and timestamps of the variable (when it's used)
-                      ;; LockTable  -> Set T to lock (never become in-place)
-		      do (setf (gethash val id2type) typ
-                               (gethash val trace-table) (list nth))))
+                      do (setf (gethash val id2type) typ (gethash val trace-table) time))
+            (loop for val in (getattr node :storage-id-dst)
+                  for typ in (getattr node :write-types)
+                  for time = `(,nth ,@(gethash val trace-table))
+                  if (id-is-input-p val base-graph) do (setf (gethash val lock-table) t)
+                    if (and (symbolp val) (null (gethash val trace-table)))
+                      do (setf (gethash val id2type) typ) (gethash val trace-table) (list nth))
+          if (and
+              (not (eql (node-type node) :Schedule-Item))
+              (not (eql (node-class node) :Render))) do    
+	        (loop for val in (node-reads node)
+		      for typ in (relay-reads (read-type-relay node))
+		      for time = `(,nth ,@(gethash val trace-table))
+                      if (id-is-input-p val base-graph) do (setf (gethash val lock-table) t)
+                        if (symbolp val)
+                          do (setf (gethash val id2type) typ (gethash val trace-table) time)) ;; (incf consume)
+	        (loop for val in (node-writes node)
+		      for typ in (relay-writes (read-type-relay node))
+                      if (id-is-input-p val base-graph) do (setf (gethash val lock-table) t)
+		        if (and (symbolp val) (null (gethash val trace-table)))
+                          ;; ID2Type    -> the variable name and its type
+                          ;; TraceTable -> the variable name and timestamps of the variable (when it's used)
+                          ;; LockTable  -> Set T to lock (never become in-place)
+		          do (setf (gethash val id2type) typ
+                                   (gethash val trace-table) (list nth))))
     (let* ((memory-blocks
 	     (loop for key in (alexandria:hash-table-keys trace-table)
 	           for typ = (gethash key id2type)
@@ -239,15 +185,12 @@ The goal of run-memory-planner is to reduce the number of :allocate-p object in 
            (alias-map (make-hash-table)))
       (loop for mb in solved
             do (setf (gethash (memoryblock-id mb) alias-map) (or (memoryblock-answer mb) (memoryblock-id mb))))
-      (labels ((newid (id)
-               (if (gethash id alias-map)
-                   (if (eql (gethash id alias-map) id)
-                       id
-                       (newid (gethash id alias-map)))
-                   id)))
+      (flet ((newid (id) (or (gethash id alias-map) id)))
         (assert (equal outputs (map 'list #'newid outputs)) () "memory-planner: the value of constants are immutable. ~a -> ~a" outputs (map 'list #'newid outputs))
-        (rewrite-bp-with-newid item #'newid t))
-      alias-map)))
+        (dolist (node (graph-nodes schedule-graph))
+          (if (getattr node :jitable)
+              (rewrite-bp-with-newid node #'newid)
+              nil))))))
 
 (defun buffer-sizeof (buffer)
   "Returns the size of the buffer in bits"
@@ -296,14 +239,7 @@ The goal of run-memory-planner is to reduce the number of :allocate-p object in 
   (let ((static-graph-p (null symbolics)))
     (multiple-value-bind (before-count before-size)
         (when (>= (ctx:getenv :JIT_DEBUG) 2) (evaluate schedule-graph static-graph-p))
-      ;; First, applying the memory-planner kernel by kernel.
-      ;; The goal is to reduce the number of arguments in the kernel.
-      (dolist (item (graph-nodes schedule-graph))
-        (when (and (getattr item :jitable) (getattr item :blueprint))
-          (run-memory-planner-local item schedule-graph symbolics base-graph)))
-      ;; Second, applying the memory-planner in the schedule-graph level
-      ;; The goal here is to reduce the number of :allocate-p object in schedule-graph.
-      (run-memory-planner-global schedule-graph symbolics base-graph)
+      (apply-memory-planner schedule-graph symbolics base-graph)
       (mapc #'remove-extra-node-writes-to (graph-nodes schedule-graph))
       (multiple-value-bind (after-count after-size)
           (when (>= (ctx:getenv :JIT_DEBUG) 2) (evaluate schedule-graph static-graph-p))
