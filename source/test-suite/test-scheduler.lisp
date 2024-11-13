daa32272ab6dcb42984cb93600499d8871b4077c
hikettei <hikettei@gmail.com>
2024-11-13 19:25:35 +0900

    more failing case is needed

--- source/test-suite/test-scheduler.lisp
+++ source/test-suite/test-scheduler.lisp
@@ -12,4 +12,23 @@
         if (getattr node :jitable)
           collect node))
 
-(print (gather-kernels (schedule-with-vars (!rand `(3 3)))))
+(defun check-kernel (schedule-graph n-count)
+  (ok (= n-count (length (gather-kernels schedule-graph))) (format nil "Scheduled ~a, expected ~a" (length (gather-kernels schedule-graph)) n-count)))
+;; Adding more failing case!, the more the better
+;; Can't we add more general tests for it?
+;; [GOAL] Eliminate bugs from the scheduler
+;; Failing ?
+(deftest test-double-reduction-separated
+  (testing "Reduction with different axes are not fused into a single kernel"
+    (let ((schedule (schedule-with-vars (!sum (!sum (make-tensor `(10 10)) :axis 0 :keepdims t) :axis 1 :keepdims t))))
+      (check-kernel schedule 2))))
+
+(deftest test-chunk-schedule
+  (let ((schedule (schedule-with-vars (apply #'!matmul (multiple-value-list (!chunk (make-tensor `(2 64 64)) 2))))))
+    (check-kernel schedule 1)
+    (let ((items (getattr (car (gather-kernels schedule)) :items)))
+      (print items))))
+
+(deftest test-view-merge-failing-case
+  (let ((schedule (schedule-with-vars (caten (!gelu (!matmul (make-tensor `(1 64 64)) (!t (make-tensor `(1 64 64)))))))))
+    (check-kernel schedule 1)))
