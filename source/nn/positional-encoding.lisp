18106ca8c3aae794d9501803adb6258f4a7a6ffb
Ayman Bourramouss <aymanbourramouss@gmail.com>
2024-11-13 23:48:10 +0100

    x1 and x2 pairs for rope

--- source/nn/positional-encoding.lisp
+++ source/nn/positional-encoding.lisp
@@ -12,65 +12,39 @@
      (offset offset)))
 
 
+
 (defmethod call ((op RoPE) &rest inputs)
   (let* ((x (car inputs))
          (shape (shape x))
          (last-two (last shape 2))
          (n (first last-two))
-         (d (second last-two)))
+         (d (second last-two))
+         (d-minus-1 (1- d)))
     (dotimes (i 1)
-      (format t "i = ~a~%" i)
       (let* ((shape (shape x))
-             (b (reduce #'* ( butlast shape 2)))
+             (b (reduce #'* (butlast shape 2)))
              (x (!reshape x (list b n d)))
              (positions (!index-components (list n)))
-              ;TODO: handle potential divisions by 0
-             (freqs (!exp (!div (!index-components (list (floor d 2))) (!const x (log (- (floor 10000 d) 1))))))
+             ;TODO: handle potential divisions by 0
+             (freqs (!exp (!div (!index-components (list (floor d 2))) 
+                                (!const x (log (- (floor 10000 d) 1))))))
              (theta (!reshape positions (list 1 n)))
              (costheta (!cos theta))
              (sintheta (!sin theta))
+             (x1 (!view x t t '(0 d 2)))
+             (x2 (!view x t t '(1 d-minus-1 2)))
              )
-        (print (proceed freqs))
-        ))))
+        (format t "~%Original x shape: ~A" (shape x))
+        (format t "~%X1 (even) shape: ~A" (shape x1))
+        (format t "~%X2 (odd) shape: ~A" (shape x2))
+        (format t "~%Freqs: ~A" (proceed freqs))
+        (values x1 x2 costheta sintheta)))))
 
 
 
-
-(defparameter *tensor1* (make-tensor `(4 6 8) :initial-element 1.0))
-
+(defparameter *tensor1* (make-tensor '(14 10 20) :initial-element 1.0))
 (let ((instance (make-instance 'RoPE)))
   (call instance *tensor1*))
-
-
-
-(defparameter *tensor* (make-tensor '(2 3 4) :initial-element 1.0))
-
-(defparameter *positions* (!index-components *tensor*))
-
-
-(print (reduce #'* ( butlast '(1 2 3 4 5) 2)))  ; Returns 120
-(print (proceed *positions*))
-
-
-(defparameter *reshaped-positions* (!reshape *positions* '(6 4)))
-
-
-(print (proceed *reshaped-positions*))
-(defparameter *freqs* (make-tensor '(1 2) :initial-element 2.0))
-
-(defparameter *theta* (!mul *reshaped-positions* *freqs*))
-
-
-(print (proceed *theta*))
-
-
-(setq shape '(5))
-
-;; Call !index-components on the shape
-(print (proceed (!index-components shape)))
-
-(print (proceed (!const *tensor1* 2)))
-(!index-components (list (shape *tensor1*)))
 
 (in-package :caten/nn.test)
 
