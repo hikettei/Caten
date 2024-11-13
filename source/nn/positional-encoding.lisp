dde647e6e5ea47f9754cac300c02502aa0a0b95f
Ayman Bourramouss <aymanbourramouss@gmail.com>
2024-11-13 21:53:35 +0100

    Update positional-encoding.lisp

--- source/nn/positional-encoding.lisp
+++ source/nn/positional-encoding.lisp
@@ -4,32 +4,67 @@
 ;; RoPE
 ;; PositionalEncoding
 
-(defmodel (RoPE (dims &key (traditional NIL) (base 10000) (scale 1.0)))
-  ((dims dims)
-   (traditional traditional)
-   (base base)
-   (scale scale)))
-
-;RoPE needs an extra arg in the call function, the generic doesn't consider it
-;offset: int = 0
 (defmethod call ((op RoPE) &rest inputs)
   (let* ((x (car inputs))
          (shape (shape x))
          (last-two (last shape 2))
          (n (first last-two))
          (d (second last-two)))
-    (dotimes (i 10)
+    (dotimes (i 1)
       (format t "i = ~a~%" i)
-      (defparameter shape (shape x))
-      (defparameter x (!reshape x (list n d)))
-      (defparameter positions (range 0 n )) ;not sure if this is what mlx doing (mx.arange(n))
-      (print positions)
-      )))
+      (let* ((shape (shape x))
+             (b (reduce #'* ( butlast shape 2)))
+             (x (!reshape x (list b n d)))
+             (positions (!index-components (list n)))
+              ;TODO: handle potential divisions by 0
+             (freqs (!exp (!div (!index-components (list (floor d 2))) (!const x (log (- (floor 10000 d) 1))))))
+             (theta (!reshape positions (list 1 n)))
+             (costheta (!cos theta))
+             (sintheta (!sin theta))
+             )
+        (print (proceed freqs))
+        ))))
+
+
+
+(defparameter *tensor1* (make-tensor `(4 6 8) :initial-element 1.0))
 
 (let ((instance (make-instance 'RoPE)))
   (call instance *tensor1*))
 
+
+
+(defparameter *tensor* (make-tensor '(2 3 4) :initial-element 1.0))
+
+(defparameter *positions* (!index-components *tensor*))
+
+
+(print (reduce #'* ( butlast '(1 2 3 4 5) 2)))  ; Returns 120
+(print (proceed *positions*))
+
+
+(defparameter *reshaped-positions* (!reshape *positions* '(6 4)))
+
+
+(print (proceed *reshaped-positions*))
+(defparameter *freqs* (make-tensor '(1 2) :initial-element 2.0))
+
+(defparameter *theta* (!mul *reshaped-positions* *freqs*))
+
+
+(print (proceed *theta*))
+
+
+(setq shape '(5))
+
+;; Call !index-components on the shape
+(print (proceed (!index-components shape)))
+
+(print (proceed (!const *tensor1* 2)))
+(!index-components (list (shape *tensor1*)))
+
 (in-package :caten/nn.test)
+
 
 
 (defun test-rope-tensor ()
