a66a3df38897c75f52cf0f3caeae69bdfe1304a1
hikettei <hikettei@gmail.com>
2024-11-16 16:53:23 +0900

    Finish writing test-suite

--- source/test-suite/test-dynamic-shape.lisp
+++ source/test-suite/test-dynamic-shape.lisp
@@ -12,40 +12,23 @@
       (let ((args (node-reads kernel)))
         (ok (equal (butlast (subseq args 1)) `(BYB TOB FROMB BYA TOA FROMA)))))))
 
-(run-test 'symbolic-function-args-test)
-
-
-
-
-#|
-(deftest symbolic-function-args-test
-  (with-protect-jit
-    (when (= (ctx:getenv :JIT) 1)
-      (let ((args (node-reads (get-jit-info (caten (!add (!view (make-tensor `(n)) `(froma toa bya)) (!view (make-tensor `(n)) `(fromb tob byb))))))))
-	(ok (every #'(lambda (x) (find x args)) `(N FROMB TOB BYB TOA FROMA BYA)))
-	(ok (= (length args) 9))))))
-
-(deftest tensor-shaped-tensor-test
+(deftest tensor-shaped-tensor-test-1
   (let* ((size1 (!add (iconst 'a) (iconst 'a)))
-	 (size2 (!add (iconst 'b) (iconst 'b)))
-	 (tensor (caten (make-tensor `(,size1 ,size2) :initial-element 'a)))
-	 (out (elements (forward tensor `(a . 4) `(b . 8)))))
+         (size2 (!add (iconst 'b) (iconst 'b)))
+         (tensor (caten (make-tensor `(,size1 ,size2) :initial-element 'a)))
+         (out (elements (forward tensor `(a . 4) `(b . 8)))))
     (ok (= (length out) 128))
-    (ok (every (equal-to 4) out)))
-  ;; segv
-  ;; (let* ((size1 (!add (iconst 'a) (iconst 'a)))
-  ;;	 (size2 (!add (iconst 'b) (iconst 'b)))
-  ;;	 (tensor (caten (!sin (make-tensor `(,size1 ,size2) :initial-element 'a))))
-  ;;	 (out (elements (forward tensor `(a . 4) `(b . 8)))))g
-  ;;  (ok (= (length out) 128))
-  ;;  (ok (every (equal-to (sin 4)) out)))
-  )
+    (ok (every #'(lambda (x) (= x 4)) out))))
 
-;; TODO: Tensor-Shaped-Tesnor Operation
-;; TODO: Tensor-Shaped-Tensor Iteration Rendering (The scalar result should be passed via arguments)
-;; segv
+(deftest tensor-shaped-tensor-test-2
+  (let* ((size1 (!add (iconst 'a) (iconst 'a)))
+  	 (size2 (!add (iconst 'b) (iconst 'b)))
+  	 (tensor (caten (!sin (make-tensor `(,size1 ,size2) :initial-element 'a))))
+  	 (out (elements (forward tensor `(a . 4) `(b . 8)))))
+    (ok (= (length out) 128))
+    (ok (every #'(lambda (x) (= x (sin 4))) out))))
 
-(deftest tensor-viewed-tensor-test
+(deftest tensor-view-tensor-test-1
   (testing "Upfrom"
     (let* ((v1 (!add (iconst 'a) (iconst 'a)))
 	   (v2 (!add (iconst 'b) (iconst 'b)))
@@ -55,17 +38,12 @@
 	   (result (forward model `(a . 1) `(b . 2))))
       (ok (equal `(8 6) (buffer-shape (tensor-buffer result))))
       (ok (= 48 (length (elements result))))
-      (ok (every (equal-to 1.0) (elements result)))))
-  (testing "Below"
+      (ok (every #'(lambda (x) (= x 1.0)) (elements result))))))
 
-    )
-  (testing "By"
+(deftest symbolic-tensor-failing-case-1
+  (let ((m (caten (!sin (make-tensor `(,(!reshape (!add (make-tensor `(1)) (iconst 'n)))))))))
+    (print (forward m `(n . 3)))))
 
-    )
-  (testing "Broadcast"
-
-;; [TODO] Minimal Repro for two failing case
-;;(caten (!sin (make-tensor `(,(!add (make-tensor `(1)) (iconst 'n))))))
-;; (caten (!add (!add (!index-components `(5)) (!add caten/apis::*rng-counter* (!* (iconst 2) (iconst 'n)) :reduce t))  (!* (iconst 2) (iconst 'n))))
-))
-|#
+(deftest symbolic-tensor-failing-case-2
+  (let ((m (caten (!add (!add (!index-components `(5)) (!add caten/apis::*rng-counter* (!* (iconst 2) (iconst 'n)) :reduce t))  (!* (iconst 2) (iconst 'n))))))
+    (print m)))
