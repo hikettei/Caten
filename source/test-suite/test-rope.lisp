a61995640fbd7e20b5ab069a61c83f719ebd816a
Ayman Bourramouss <aymanbourramouss@gmail.com>
2024-11-20 13:51:53 +0100

    fix freqs
    Co-Authored-By: hikettei <88639579+hikettei@users.noreply.github.com>

--- source/test-suite/test-rope.lisp
+++ source/test-suite/test-rope.lisp
@@ -10,6 +10,14 @@ def torch_rope(x):
     seq_len = x.size(1)
     seq_idx = torch.arange(seq_len, dtype=theta.dtype, device=theta.device)
     idx_theta = torch.einsum('i,j->ij', seq_idx, theta)
+    costheta = torch.cos(idx_theta)
+    sintheta = torch.sin(idx_theta)
+    x_shaped = x.float().reshape(*x.shape[:-1], -1, 2)
+    x1 = x_shaped[..., 0]
+    x2 = x_shaped[..., 1]
+    rx1 = x1 * costheta - x2 * sintheta
+    rx2 = x1 * sintheta + x2 * costheta
+    rx = torch.stack([rx1, rx2], dim=-1)
     cache = torch.stack([torch.cos(idx_theta), torch.sin(idx_theta)], dim=-1)
     x_shaped = x.float().reshape(*x.shape[:-1], -1, 2)
     rope_cache = cache.view(1, seq_len, 1, x_shaped.size(-2), 2)
@@ -22,6 +30,16 @@ def torch_rope(x):
 
 (import-function "torch_rope")
 
+
+
+
+(let ((x (rand `(1 20 20 20))))
+   (with-torch (x) (->caten (torch_rope x)))
+   (proceed (!rope x))))))
+
+
+
+
 (deftest test-rope
   (with-given-dtype ((:float32 . "float32"))
     (let ((x (rand `(1 20 20 20))))
@@ -29,3 +47,4 @@ def torch_rope(x):
        (:atol 1e-5 :rtol 1e-6)
        (with-torch (x) (->caten (torch_rope x)))
        (proceed (!rope x))))))
+
