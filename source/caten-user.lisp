47ebe2a24a1a6e37327c7da49538b503c8b39817
hikettei <hikettei@gmail.com>
2024-11-24 20:24:16 +0900

    more docs

--- source/caten-user.lisp
+++ source/caten-user.lisp
@@ -1,5 +1,6 @@
 (defpackage :caten-user
   (:documentation "REPL Playground for Caten")
   (:use :cl :caten :caten/aasm :caten/air :caten/codegen :caten/nn :caten/air
-        :caten/avm :caten/common.documentation))
+   :caten/avm :caten/common.documentation
+   :caten/codegen/scheduler :caten/codegen/shape-inference :caten/codegen/rewriting-rules))
 (in-package :caten-user)
--- source/codegen/documentation.lisp
+++ source/codegen/documentation.lisp
@@ -49,12 +49,36 @@ JIT_DEBUG | Effect
 3     | Displays schedule-graph
 4     | Displays memory-planner and the compiled code
 ")
+  (title "How to learn JIT")
+  (body "You can start by reading `./caten/docs/getting-started.lisp` to understand the basic concepts of `caten/codegen` through a running example.
+
+Additionally, the following sections provide documentation and explanations for each JIT mechanism, along with simple executable examples.")
   (title "Shape Inference")
-  
+  (body "The JIT compilation starts by inferring the shape of each points in the graph. `caten/codegen/shape-inference` is responsible for this task.")
+  (doc/function "run-type-infer" #'caten/codegen/shape-inference:run-type-infer)
+  (doc/struct "Inferred-Type" 'caten/codegen/shape-inference:Inferred-Type)
+  (doc/struct "Iteration-Space" 'caten/codegen/shape-inference:Iteration-Space)
+  ;; [TODO] Provide an example
   (title "Rewriting Rules")
+  (body "TODO: Some patterns")
+
+  (subtitle "Optimization for gemm")
+  (body "TODO: WMMA Rewriting")
   
   (title "Scheduler")
-  
+  (doc/package 'caten/codegen/scheduler)
+  (doc/function "graph-schedule" #'caten/codegen/scheduler:graph-schedule)
+  (subtitle "Example (Scheduler + Shape Inference + Rewriting Rules")
+  (body "This code snippet demonstrates how to create a schedule-graph from AASM Graph. AASM Graph is obtained by running caten with JIT=0.")
+  (example-repl "(ctx:with-contextvar (:JIT 0) (pprint-graph (avm-graph (caten (!relu (!matmul (make-tensor `(3 3)) (make-tensor `(3 3))))))))")
+  (example-code "
+(let* ((graph (ctx:with-contextvar (:JIT 0) (avm-graph (caten (!relu (!matmul (make-tensor `(3 3)) (make-tensor `(3 3))))))))
+       (vm (make-avm graph :example nil (graph-outputs graph) nil)))
+  (run-type-infer vm) ;; Running the shape inference
+  (apply-rewriting-rules vm) ;; Running the rewriting rules
+  ;; graph-schedule to finally run the scheduler
+  (pprint-graph (graph-schedule (avm-graph vm))))
+")
   (title "Lowerer (Blueprint)")
 
   (title "Renderer")
