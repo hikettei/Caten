962a37d0c8df8db87c55aabca3d607ec1c14df96
hikettei <88639579+hikettei@users.noreply.github.com>
2024-11-29 14:03:45 +0900

    Feat: defcall (#268)

--- external/llm/layers.lisp
+++ external/llm/layers.lisp
@@ -54,10 +54,9 @@
     ((c-fc   (Linear dim hidden-dim))
      (c-proj (Linear hidden-dim dim))))
 
-(defmethod call ((model FeedForward) &rest inputs)
-  (multiple-value-bind (x) (apply #'values inputs)
-    (with-slots ((c-fc c-fc) (c-proj c-proj)) model
-      (forward c-proj (!gelu (forward c-fc x))))))
+(defcall (model FeedForward) (X[~])
+  (with-slots ((c-fc c-fc) (c-proj c-proj)) model
+    (forward c-proj (!gelu (forward c-fc x)))))
 
 (defmodel (TransformerBlock (dim n-heads &key (norm-eps 1e-5) (max-seq-len 1024)))
     ((attn (Attention dim n-heads max-seq-len))
@@ -79,19 +78,14 @@
      (ln-f (LayerNorm `(,dim) :eps norm-eps))
      (lm-head (Linear dim vocab-size :bias nil))))
 
-(defmethod call ((model Transformer) &rest inputs)
-  (multiple-value-bind (tokens start-pos) (apply #'values inputs)
-    (assert (and tokens start-pos))
-    (st "Tokens[batch sentence_length] Start_Pos[] -> Tokens[batch sentence_length]" (tokens start-pos))
-    ;; (assert (numberp (second (shape tokens))) () "The second dimension of the tensor (seq_len) must be a fixnum, getting ~a" (shape tokens))
-    (with-slots ((wte wte) (wpe wpe) (h h) (ln-f ln-f) (lm-head lm-head)) model
-      (let* ((token-emb (forward wte tokens))
-	     (pos-emb   (forward wpe (!cast (!add start-pos (!index-components `(1 ,(second (shape tokens))))) (dtype-of tokens))))
-	     (hi (!add token-emb pos-emb))
-             (seq-len (iconst (second (shape tokens))))
-	     (mask (!triu (!full `(1 1 ,seq-len ,(!+ start-pos (iconst seq-len))) (-inf)) :diagonal (!+ (iconst 1) start-pos)))
-	     (_ (dolist (hn h) (setf hi (forward hn hi mask start-pos))))
-	     (logits (forward lm-head (forward ln-f hi))))
-	(declare (ignore _))
-	;; (!argmax (!view logits t -1 t))
-        (!argmax logits)))))
+(defcall (model Transformer) (Tokens[Batch Seq-Len] Start-Pos[])
+  (with-slots ((wte wte) (wpe wpe) (h h) (ln-f ln-f) (lm-head lm-head)) model
+    (let* ((token-emb (forward wte tokens))
+	   (pos-emb   (forward wpe (!cast (!add start-pos (!index-components `(1 ,seq-len))) (dtype-of tokens))))
+	   (hi (!add token-emb pos-emb))
+	   (mask (!triu (!full `(1 1 ,seq-len ,(!+ start-pos (iconst seq-len))) (-inf)) :diagonal (!+ (iconst 1) start-pos)))
+	   (_ (dolist (hn h) (setf hi (forward hn hi mask start-pos))))
+	   (logits (forward lm-head (forward ln-f hi))))
+      (declare (ignore _))
+      ;; (!argmax (!view logits t -1 t))
+      (!argmax logits))))
