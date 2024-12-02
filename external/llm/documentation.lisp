2d04a7b1daef0af329f816164898093e7fe12503
hikettei <hikettei@gmail.com>
2024-12-03 02:13:27 +0900

    wip

--- external/llm/documentation.lisp
+++ external/llm/documentation.lisp
@@ -5,6 +5,28 @@
 
 (define-page ("caten/llm" "packages/caten.external.llm.md")
   (title "Overview")
-  (body "Implements Tokenizer, models")
-  (title "Tokenizers")
-  (title "Models"))
+  (body "TODO: Docs")
+  (subtitle "Tokenizers")
+  (body "TODO: Docs")
+  (doc/class "Tokenizer" 'caten/llm:Tokenizer)
+  (doc/class "BPETokenizer" 'caten/llm:BPETokenizer)
+  (doc/generic "encode" 'caten/llm:encode)
+  (doc/generic "decode" 'caten/llm:decode)
+  (subtitle "Models")
+  (body "
+(TODO: Complete docs)
+
+Definitions are here: https://github.com/hikettei/Caten/blob/main/external/llm/layers.lisp
+
+Currently, the following models are implemented and tested:
+
+- Attention
+
+- Scaled-Dot-Product-Attention
+
+- FeedForward
+
+- TransformerBlock
+
+- Transformer
+"))
