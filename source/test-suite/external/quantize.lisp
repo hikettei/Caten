a6b900e978499705fd69401da2431b00c3de00b0
Ayman Bourramouss <aymanbourramouss@gmail.com>
2024-12-03 17:04:58 +0100

    fix tabs, use gguf in deftests

--- source/test-suite/external/quantize.lisp
+++ source/test-suite/external/quantize.lisp
@@ -2,11 +2,6 @@
 
 (python-exec
 "
-
-import numpy as np
-from gguf import GGUFWriter, GGMLQuantizationType
-from gguf.quants import quantize, GGML_QUANT_SIZES
-
 def write_dummy_gguf(quantization_type, filename):
     data = np.arange(64, dtype=np.float32)
     gguf_writer = GGUFWriter(filename, \"test_arch\")
@@ -37,47 +32,38 @@ def write_dummy_gguf(quantization_type, filename):
 "
 def read_gguf_file(gguf_file_path):
     reader = GGUFReader(gguf_file_path)
-    tensors_data = {}
-    results = []
     for tensor in reader.tensors:
         if tensor.tensor_type != GGMLQuantizationType.F32 and tensor.tensor_type != GGMLQuantizationType.F16:
-            data = dequantize(tensor.data, tensor.tensor_type)
+            data = dequantize(tensor.data, tensor.tensor_type).tolist()
         else:
-            data = tensor.data
-        tensors_data[tensor.name] = data
-        results.append(data)
+            data = tensor.data.tolist()
 
-    return results")
+    return data
+")
 
 (import-function "write_dummy_gguf")
 (import-function "read_gguf_file")
 
-
-(write_dummy_gguf "dummy.gguf" "F16")
-
-(print (change-facet (read_gguf "dummy.gguf") :tensor))
-
-(print (change-facet (caten/gguf:tensor-info-buffer (car (caten/gguf:gguf-tensor-info (caten/gguf:load-gguf "dummy.gguf")))) :tensor)))))
 (deftest test-dequantization-q8_0
-(generate_dummy_gguf "dummy.gguf" "Q8_0")
-(with-given-dtype ((:float32 . "float32"))
-  (assert-equal
-   (:atol 1e-5 :rtol 1e-6)
-   (change-facet (read_gguf "dummy.gguf") :tensor)
-   (change-facet (caten/gguf:tensor-info-buffer (car (caten/gguf:gguf-tensor-info (caten/gguf:load-gguf "dummy.gguf")))) :tensor))))
-
-(deftest test-dequantization-f16
-  (generate_dummy_gguf "dummy.gguf" "F16")
+  (write_dummy_gguf "q8_0" "dummy.gguf")
   (with-given-dtype ((:float32 . "float32"))
     (assert-equal
      (:atol 1e-5 :rtol 1e-6)
-     (change-facet (read_gguf "dummy.gguf") :tensor)
+     (change-facet (read_gguf_file "dummy.gguf") :tensor)
+     (change-facet (caten/gguf:tensor-info-buffer (car (caten/gguf:gguf-tensor-info (caten/gguf:load-gguf "dummy.gguf")))) :tensor))))
+
+(deftest test-dequantization-f16
+  (write_dummy_gguf "f16" "dummy.gguf")
+  (with-given-dtype ((:float32 . "float32"))
+    (assert-equal
+     (:atol 1e-5 :rtol 1e-6)
+     (change-facet (read_gguf_file "dummy.gguf") :tensor)
      (change-facet (caten/gguf:tensor-info-buffer (car (caten/gguf:gguf-tensor-info (caten/gguf:load-gguf "dummy.gguf")))) :tensor))))
 
 (deftest test-dequantization-f32
-(generate_dummy_gguf "dummy.gguf" "F32")
-(with-given-dtype ((:float32 . "float32"))
-  (assert-equal
-   (:atol 1e-5 :rtol 1e-6)
-   (change-facet (read_gguf "dummy.gguf") :tensor)
-   (change-facet (caten/gguf:tensor-info-buffer (car (caten/gguf:gguf-tensor-info (caten/gguf:load-gguf "dummy.gguf")))) :tensor))))
+  (write_dummy_gguf "f32" "dummy.gguf")
+  (with-given-dtype ((:float32 . "float32"))
+    (assert-equal
+     (:atol 1e-5 :rtol 1e-6)
+     (change-facet (read_gguf_file "dummy.gguf") :tensor)
+     (change-facet (caten/gguf:tensor-info-buffer (car (caten/gguf:gguf-tensor-info (caten/gguf:load-gguf "dummy.gguf")))) :tensor))))
