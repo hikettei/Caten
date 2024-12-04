7b99f449154b0018a7d7957d8fa82b91bcc2e524
Ayman Bourramouss <aymanbourramouss@gmail.com>
2024-12-04 06:06:17 +0100

    dequantization: int8 to fp32 (#267)
    Co-authored-by: hikettei <88639579+hikettei@users.noreply.github.com>

--- source/test-suite/package.lisp
+++ source/test-suite/package.lisp
@@ -10,9 +10,13 @@
 (in-package :caten/test-suite)
 
 (python-exec "import torch
+import struct
+import gguf
 import torch.nn.functional as f
 import numpy as np")
 (import-module "numpy" :as "np" :reload t)
+(import-module "struct" :reload t)
+(import-module "gguf" :as "gguf" :reload t)
 (import-module "torch.nn.functional" :as "f" :reload t)
 (import-function "torch.from_numpy")
 (import-function "torch.matmul")
