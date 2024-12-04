101676529afc7f7c38a020ec72c1aa4d9cc1ba61
hikettei <hikettei@gmail.com>
2024-12-04 23:36:34 +0900

    more tests on pool

--- source/test-suite/test-pool.lisp
+++ source/test-suite/test-pool.lisp
@@ -19,4 +19,45 @@
             (with-torch (input)
               (->caten (f:max_pool2d input `(2 2))))
             (proceed (!maxpool input :kernel-size `(2 2))))))))
+
+
+(deftest test-avg-pooling-padded
+  (with-given-dtype ((:float32 . "float32"))
+    (with-no-grad
+      (let ((input (rand `(10 3 25 25))))
+        (assert-equal
+            (:atol 1e-5 :rtol 1e-5)
+            (with-torch (input)
+              (->caten (f:avg_pool2d input `(4 4) :padding 2)))
+            (proceed (!avgpool input :kernel-size `(4 4) :padding 2)))))))
+
+(deftest test-max-pooling-padded
+  (with-given-dtype ((:float32 . "float32"))
+    (with-no-grad
+      (let ((input (rand `(10 3 25 25))))
+        (assert-equal
+            (:atol 1e-5 :rtol 1e-5)
+            (with-torch (input)
+              (->caten (f:max_pool2d input `(4 4) :padding 2)))
+            (proceed (!maxpool input :kernel-size `(4 4) :padding 2)))))))
+
+(deftest test-avg-pooling-strided
+  (with-given-dtype ((:float32 . "float32"))
+    (with-no-grad
+      (let ((input (rand `(10 3 25 25))))
+        (assert-equal
+            (:atol 1e-5 :rtol 1e-5)
+            (with-torch (input)
+              (->caten (f:avg_pool2d input `(4 4) :stride 2)))
+            (proceed (!avgpool input :kernel-size `(4 4) :stride 2)))))))
+
+(deftest test-max-pooling-strided
+  (with-given-dtype ((:float32 . "float32"))
+    (with-no-grad
+      (let ((input (rand `(10 3 25 25))))
+        (assert-equal
+            (:atol 1e-5 :rtol 1e-5)
+            (with-torch (input)
+              (->caten (f:max_pool2d input `(4 4) :stride 2)))
+            (proceed (!maxpool input :kernel-size `(4 4) :stride 2)))))))
 ;; [TODO] Scheduling Test: Max/Avg Pooling should be a single kernel
