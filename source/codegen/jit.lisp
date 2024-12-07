d4d6f1332336d31e92ca97b4f31eb76b76306637
hikettei <hikettei@gmail.com>
2024-12-07 18:37:24 +0900

    ?

--- source/codegen/jit.lisp
+++ source/codegen/jit.lisp
@@ -150,7 +150,7 @@ caten/codegen overview:
              (append
               (getattr si :storage-id-dst) ;; optimized by memory-planner
               (map 'list #'car (getattr si :dynamic-shapes))
-              (node-reads si))
+              (getattr si :storage-id-src)) ;; optimized by memory-planner
              :output-buffer-n (length (node-writes si))
              :kernel-info (make-compiled-kernel-from-si si graph)
              :dtypes
