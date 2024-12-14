399c2fc3490ea574dc6742d9dbc601a3b810beee
hikettei <hikettei@gmail.com>
2024-12-14 20:08:30 +0900

    self defined write should not be an read

--- source/codegen/blueprint.lisp
+++ source/codegen/blueprint.lisp
@@ -549,12 +549,14 @@ Lowers the Schedule-Item into blueprint.
             (ctx-blueprint ctx) (blueprint-exprify (ctx-blueprint ctx) node) ; rewrite jitable nodes -> expr
             (ctx-blueprint ctx) (ctx-padding-loop ctx)) ;; keep the rank of loops same
       (multiple-value-bind (new-bp id-as-dag-map) (blueprint-propagate-reduction (ctx-blueprint ctx)) ;; A = B + C * D => B += C * D
+        ;; id-as-dag-map: subsequent kernels will recognise the key as value
         (setf (ctx-blueprint ctx) new-bp)
         ;; Synchronize the realized buffers
         (multiple-value-bind (writes reads constants) (blueprint-realized-buffers (ctx-blueprint ctx) node)
-          (let ((before-assigned-map (loop for w in writes
-                                           if (gethash (car w) id-as-dag-map)
-                                             collect (car w))))
+          (let ((before-assigned-map) (cycle))
+            (loop for w in writes
+                  if (and (gethash (car w) id-as-dag-map)) do
+                    (if (find (car w) (node-writes node)) (push (car w) cycle) (push (car w) before-assigned-map)))
             (setf (getattr node :read-types) (map 'list #'cdr reads)
                   (getattr node :write-types) (map 'list #'cdr writes)
                   (getattr node :storage-id-src) (map 'list #'car reads)
@@ -562,7 +564,7 @@ Lowers the Schedule-Item into blueprint.
                   (getattr node :dynamic-shapes) constants
                   (node-reads node) (append before-assigned-map (map 'list #'car reads))
                   ;; If A is rewritten as B by the propagate-reduction, other items still recognise A as A.
-                  (node-writes node) (map 'list #'(lambda (x) (or (gethash (car x) id-as-dag-map) (car x))) writes)))))
+                  (node-writes node) (append cycle (map 'list #'(lambda (x) (or (gethash (car x) id-as-dag-map) (car x))) writes))))))
       (blueprint-set-iterations (ctx-blueprint ctx)) ;; Finalize the iteration space
       (setf (getattr node :blueprint) (ctx-blueprint ctx)))))
 ;; ~~~ Schedule Cache ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
