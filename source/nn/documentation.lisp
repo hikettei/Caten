3edd254318996e5c682463984cd51f733a4beb11
hikettei <hikettei@gmail.com>
2024-12-15 15:58:34 +0900

    docs

--- source/nn/documentation.lisp
+++ source/nn/documentation.lisp
@@ -4,3 +4,34 @@
   (docs:title "caten/nn")
   (docs:body "TODO"))
 
+(docs:define-page ("Activations" "packages/caten.nn.activations.md")
+  (docs:title "Activations")
+  (docs:body "TODO"))
+
+(docs:define-page ("Convolutions" "packages/caten.nn.convolutions.md")
+  (docs:title "Convolutions")
+  (docs:body "TODO"))
+
+(docs:define-page ("Embeddings" "packages/caten.nn.embeddings.md")
+  (docs:title "Embeddings")
+  (docs:body "TODO"))
+
+(docs:define-page ("Linear" "packages/caten.nn.linears.md")
+  (docs:title "Linear"))
+
+(docs:define-page ("Normalizations" "packages/caten.nn.normalizations.md")
+  (docs:title "Normalizations")
+  (docs:body "TODO"))
+
+(docs:define-page ("Padding" "packages/caten.nn.padding.md")
+  (docs:title "Padding")
+  (docs:body "TODO"))
+
+(docs:define-page ("Pooling" "packages/caten.nn.pooling.md")
+  (docs:title "Pooling")
+  (docs:body "TODO"))
+
+(docs:define-page ("Encoding" "packages/caten.nn.encoding.md")
+  (docs:title "Encoding")
+  (docs:body "TODO"))
+
