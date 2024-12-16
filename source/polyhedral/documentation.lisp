81b1037285b7c33d5c161f3786ece373802003c4
hikettei <hikettei@gmail.com>
2024-12-17 00:43:13 +0900

    doc

--- source/polyhedral/documentation.lisp
+++ source/polyhedral/documentation.lisp
@@ -5,13 +5,18 @@
 
 (define-page ("caten/polyhedral" "packages/caten.polyhedral.md")
   (title "caten/polyhedral")
-  (subtitle "Automatic Parameter Scheduler (Coming soon!)")
+  (subtitle "Advanced Auto Scheduler (Coming soon!)")
   (body "caten/polyhedral will provide a customizable polyhedral compiler for various backends
 
-The goal is:
-- Auto-Packing for Vectorization
-- Finding the best tiling
-- auto parallelization
+This package will also provide the following advanced auto scheduling algorithm compared to `caten/codegen`.
+
+- [ ] Maximizing the outermost loop coincidence
+- [ ] Tiling
+- [ ] Packing
+- [ ] Classical Loop Transformation (e.g.: Loop Permutation, Loop Interchange, Loop Unrolling, etc
+- [ ] Complicated Kernel Fusion (e.g.: Matmul+Normalization, ConvND+Activation+Pooling, etc)
+
+...
 
 To enable this, set `AUTO_SCHEDULER=1`.
 "))
