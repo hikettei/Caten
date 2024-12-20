e32cec23aff343bdfc6e491aff0198bde03c0750
hikettei <hikettei@gmail.com>
2024-12-20 20:48:52 +0900

    tiling all bands

--- source/polyhedral/tiling.lisp
+++ source/polyhedral/tiling.lisp
@@ -58,39 +58,17 @@
       (schedule-node-get-schedule
        (schedule-node-insert-partial-schedule band tiled-sched)))))
 
-(defun get-tileable-bands (schedule)
-  (declare (type schedule schedule))
-  (let ((node (schedule-get-root schedule))
-        (next-nodes)
-        (tileable-bands))
-    ;; Enumerate all tilable bands
-    (loop named tiling-search
-          for n-children = (isl::%isl-schedule-node-n-children (isl::schedule-node-handle node))
-          while (> n-children 0) do
-            ;; Reached to the maximum tile band size => Stop
-            (loop named search-for-children
-                  for nth upfrom 0 below n-children
-                  for band = (schedule-node-get-child node nth)
-                  for type = (schedule-node-get-type band)
-                  if (eql type :schedule-node-band)
-                    do (push band tileable-bands)
-                       (push (schedule-node-get-child band 0) next-nodes)
-                       (return-from search-for-children)
-                  else
-                    do (push band next-nodes))
-            (when (= (length next-nodes) 0)
-              (return-from tiling-search))
-            (setf node (pop next-nodes)))
-    tileable-bands))
+(defun get-tileable-bands (poly)
+  (map-schedule-nodes #'(lambda (type node) (when (eql type :schedule-node-band) node)) poly))
 
 (defun tile-bands (scheduler ir)
   "`tile-bands` helps you execute the computation tile by tile over the two axes"
   (declare (type Polyhedral-IR ir))
-  (let* ((schedule (poly-schedule ir))
-         (bands (get-tileable-bands schedule)))
+  (let* ((bands (get-tileable-bands ir)))
     (when (not (= 0 (auto-scheduler-tile-size scheduler)))
+      ;; Tile all bands
       (dotimes (i (length bands))
         (setf (poly-schedule ir)
               (schedule-tile-band
-               (nth i (get-tileable-bands (poly-schedule ir)))
+               (nth i (get-tileable-bands ir))
                :size-default (auto-scheduler-tile-size scheduler)))))))
