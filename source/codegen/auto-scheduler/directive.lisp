d2de5a1f3e6d119900486e327ca18c6f94d70133
hikettei <hikettei@gmail.com>
2024-12-21 19:47:31 +0900

    Tweak

--- source/codegen/auto-scheduler/directive.lisp
+++ source/codegen/auto-scheduler/directive.lisp
@@ -1,10 +1,10 @@
 (defpackage :caten/codegen/directive
   (:documentation "Provides various hand-written blueprint rewriting rule for marks.")
-  (:use :cl :caten/codegen/expr))
+  (:use :cl :caten/codegen/expr :caten/codegen/polyhedral-ast))
 
 (in-package :caten/codegen/directive)
 
 (defun compute-reminder-for-unroll (upfrom below by n-unroll)
-  "A helper function. (values reminder-start reminder-end)"
+  "Renders a loop for the reminder part"
   
   )
--- source/codegen/auto-scheduler/unroll.lisp
+++ source/codegen/auto-scheduler/unroll.lisp
@@ -11,43 +11,40 @@
 
 (defun mark-unroll-p (mark)
   (declare (type string mark))
-  (scan "UNROLL\\[" (string-upcase mark)))
+  (scan "UNROLL" (string-upcase mark)))
 
-(defun make-unroll (n-unroll)
+(defun make-unroll (name n-unroll)
   (declare (type integer n-unroll))
-  (format nil "UNROLL[~D]" n-unroll))
+  (format nil "~a[~D]" name n-unroll))
 
 (defun parse-unroll-directive (directive)
   (declare (type string directive))
   (assert (mark-unroll-p directive) () "The directive ~a is not an unroll" directive)
   (parse-integer (subseq directive 7 (1- (length directive)))))
 
-(defun schedule-node-band-apply-unroll (schedule-node directive &key (n-unroll 4))
+(defun schedule-node-band-apply-unroll (schedule-node inner-directive n-unroll)
   (declare (type isl::schedule-node schedule-node))
   (let* ((tiled-schedule (isl:schedule-node-band-tile schedule-node (tiling-sizes schedule-node :size-default n-unroll)))
-         (tiled-schedule
-           (isl:schedule-node-insert-mark
-            (isl:schedule-node-get-child tiled-schedule 0)
-            (isl::make-id-from-str directive))))
+         (tiled-schedule (isl::schedule-node-insert-mark (isl::schedule-node-get-child tiled-schedule 0) (isl::make-id-from-str inner-directive))))
     (isl:schedule-node-get-schedule tiled-schedule)))
 
-(defun get-packable-bands (si directive)
+(defun get-packable-bands (si directive-p)
+  (declare (type function directive-p))
   (map-schedule-nodes
    #'(lambda (type node mark)
-       (when (and (eql type :schedule-node-band)
-                  (or (null mark) (not (equalp directive (princ-to-string mark)))))
+       (when (and (eql type :schedule-node-band) (or (null mark) (not (funcall directive-p (princ-to-string mark)))))
          node))
    si))
 
-(defun schedule-apply-schedule-option (si n-unroll directive)
+(defun schedule-apply-schedule-option (si n-unroll inner-directive directive-p)
   (declare (type Polyhedral-IR si))
-  (let ((bands (get-packable-bands si directive)))
+  (let ((bands (get-packable-bands si directive-p)))
     (dotimes (nth (length bands))
-      (setf (poly-schedule si) (schedule-node-band-apply-unroll (nth nth (get-packable-bands si directive)) directive :n-unroll n-unroll)))))
+      (setf (poly-schedule si)
+            (schedule-node-band-apply-unroll (nth nth (get-packable-bands si directive-p)) inner-directive n-unroll)))))
 ;; Generic Implementation which can be applied for UNROLL, VECTORIZE
-(defun apply-packed-funcall (schedule-node unroll-by directive)
-  "Groups the iteration into several packed-funcall.
-Packed-Funcall can be also transformed into Unrolling, or Vectorizing.
+(defun apply-packed-funcall (schedule-node unroll-by inner-directive directive-p)
+  "Groups the iteration into several packed-funcall. Packed-Funcall can be also transformed into Unrolling, or Vectorizing.
 For example, the following code:
 ```
 for (int i=0; i<a; i++) {
@@ -55,6 +52,15 @@ for (int i=0; i<a; i++) {
 }
 ```
 is mutated into:
+```
+for (int i=0; i<a; i+=UNROLL_BY) {
+  // Mark(Inner_Directive)
+  for (int j=0; min(UNROLL_BY, i % UNROLL_BY); j++) {
+    T0(c0+j);
+  }
+}
+```
+`caten/codegen/directives` should have an ability to transform the marked loops into the expected form, for example, loop unrolling is mutated as:
 ```
 for (int i=0; i<(a-UNROLL_BY); i+=UNROLL_BY) {
         [packed_funcall]
@@ -64,14 +70,13 @@ for (int i=0; i<(a-UNROLL_BY); i+=UNROLL_BY) {
                  { T0(c0+3)
 }
 for (int i=a - (mod a UNROLL_BY); i<a; i+=1) {
- T0(c0) // Loop Reminder (TODO: Optimize Index Computation)
+ T0(c0)
 }
 ```
-The unrolled loop is marked as directive, so the final transformation processes can be placed in polyhedral-ast.lisp
 "
-  (declare (type node schedule-node) (type integer unroll-by) (type string directive))
-  (schedule-apply-schedule-option (getattr schedule-node :polyhedral) unroll-by directive))
+  (declare (type node schedule-node) (type integer unroll-by) (type string inner-directive) (type function directive-p))
+  (schedule-apply-schedule-option (getattr schedule-node :polyhedral) unroll-by inner-directive directive-p))
 
 (defun apply-unroll (schedule-node unroll-by)
   (declare (type node schedule-node) (type integer unroll-by))
-  (apply-packed-funcall schedule-node unroll-by "UNROLL"))
+  (apply-packed-funcall schedule-node unroll-by (make-unroll "UNROLL" unroll-by) #'mark-unroll-p))
