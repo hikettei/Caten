cd01fe9ddb6389d076c80d60c25dd4cccc335835
hikettei <hikettei@gmail.com>
2024-12-21 19:56:22 +0900

    nested unroll directive

--- source/codegen/auto-scheduler/directive.lisp
+++ source/codegen/auto-scheduler/directive.lisp
@@ -1,10 +1,11 @@
 (defpackage :caten/codegen/directive
   (:documentation "Provides various hand-written blueprint rewriting rule for marks.")
-  (:use :cl :caten/codegen/expr :caten/codegen/polyhedral-ast))
+  (:use :cl :caten/codegen/expr :caten/codegen/polyhedral-ast :caten/codegen/unroll))
 
 (in-package :caten/codegen/directive)
 
-(defun compute-reminder-for-unroll (upfrom below by n-unroll)
-  "Renders a loop for the reminder part"
-  
-  )
+(defun compute-reminder-for-unroll (mark upfrom below by n-unroll)
+  "Creates an AstFor for the reminder part of the unrolled directive"
+  (let ((n-unroll (parse-unroll-directive mark)))
+    
+    ))
--- source/codegen/auto-scheduler/unroll.lisp
+++ source/codegen/auto-scheduler/unroll.lisp
@@ -22,9 +22,11 @@
   (assert (mark-unroll-p directive) () "The directive ~a is not an unroll" directive)
   (parse-integer (subseq directive 7 (1- (length directive)))))
 
-(defun schedule-node-band-apply-unroll (schedule-node inner-directive n-unroll)
+(defun schedule-node-band-apply-unroll (schedule-node parent-directive inner-directive n-unroll)
   (declare (type isl::schedule-node schedule-node))
-  (let* ((tiled-schedule (isl:schedule-node-band-tile schedule-node (tiling-sizes schedule-node :size-default n-unroll)))
+  (let* ((tiled-schedule (isl::schedule-node-insert-mark schedule-node (isl::make-id-from-str parent-directive)))
+         (tiled-schedule (isl:schedule-node-get-child tiled-schedule 0))
+         (tiled-schedule (isl:schedule-node-band-tile tiled-schedule (tiling-sizes schedule-node :size-default n-unroll)))
          (tiled-schedule (isl::schedule-node-insert-mark (isl::schedule-node-get-child tiled-schedule 0) (isl::make-id-from-str inner-directive))))
     (isl:schedule-node-get-schedule tiled-schedule)))
 
@@ -36,14 +38,13 @@
          node))
    si))
 
-(defun schedule-apply-schedule-option (si n-unroll inner-directive directive-p)
+(defun schedule-apply-schedule-option (si n-unroll parent-directive inner-directive directive-p)
   (declare (type Polyhedral-IR si))
-  (let ((bands (get-packable-bands si directive-p)))
-    (dotimes (nth (length bands))
-      (setf (poly-schedule si)
-            (schedule-node-band-apply-unroll (nth nth (get-packable-bands si directive-p)) inner-directive n-unroll)))))
+  (loop for bands = (get-packable-bands si directive-p)
+        while bands do
+          (setf (poly-schedule si) (schedule-node-band-apply-unroll (car bands) parent-directive inner-directive n-unroll))))
 ;; Generic Implementation which can be applied for UNROLL, VECTORIZE
-(defun apply-packed-funcall (schedule-node unroll-by inner-directive directive-p)
+(defun apply-packed-funcall (schedule-node unroll-by parent-directive inner-directive directive-p)
   "Groups the iteration into several packed-funcall. Packed-Funcall can be also transformed into Unrolling, or Vectorizing.
 For example, the following code:
 ```
@@ -53,6 +54,7 @@ for (int i=0; i<a; i++) {
 ```
 is mutated into:
 ```
+// Mark(Parent_Directive)
 for (int i=0; i<a; i+=UNROLL_BY) {
   // Mark(Inner_Directive)
   for (int j=0; min(UNROLL_BY, i % UNROLL_BY); j++) {
@@ -75,8 +77,9 @@ for (int i=a - (mod a UNROLL_BY); i<a; i+=1) {
 ```
 "
   (declare (type node schedule-node) (type integer unroll-by) (type string inner-directive) (type function directive-p))
-  (schedule-apply-schedule-option (getattr schedule-node :polyhedral) unroll-by inner-directive directive-p))
+  (assert (and (funcall directive-p parent-directive) (funcall directive-p inner-directive)) () "directive-p should detect both parent and inner directive")
+  (schedule-apply-schedule-option (getattr schedule-node :polyhedral) unroll-by parent-directive inner-directive directive-p))
 
 (defun apply-unroll (schedule-node unroll-by)
   (declare (type node schedule-node) (type integer unroll-by))
-  (apply-packed-funcall schedule-node unroll-by (make-unroll "UNROLL" unroll-by) #'mark-unroll-p))
+  (apply-packed-funcall schedule-node unroll-by (make-unroll "UNROLL_PARENT" unroll-by) (make-unroll "UNROLL_BODY" unroll-by) #'mark-unroll-p))
