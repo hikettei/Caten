4e7ad65de07ccc6dab7ba7d33f961e42912b5b67
hikettei <hikettei@gmail.com>
2024-12-28 20:39:01 +0900

    moved to byoc

--- source/codegen/backends/clang.lisp
+++
@@ -1,8 +0,0 @@
-(defpackage :caten/codegen/backends/clang
-  (:use :cl :caten/air :cffi :caten/codegen/renderer :caten/codegen/helpers
-   :caten/codegen/shape-inference :caten/runtime :caten/codegen/expr)
-  (:import-from
-   :caten/codegen/config
-   #:define-auto-scheduler))
-
-(in-package :caten/codegen/backends/clang)
