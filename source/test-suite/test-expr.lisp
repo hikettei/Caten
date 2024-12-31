169ad2a70d0d850fdbe29e4ec2bb7b25d5917143
hikettei <hikettei@gmail.com>
2024-12-31 16:58:29 +0900

    ok with that style

--- source/test-suite/test-expr.lisp
+++ source/test-suite/test-expr.lisp
@@ -1,8 +1,9 @@
 (defpackage :caten/test-suite/expr
   (:documentation "A test suite for caten/codegen/expr and gflops computation.")
-  (:use :cl :rove :caten/codegen/expr))
+  (:use :cl :rove :caten/codegen/expr :caten/runtime))
 (in-package :caten/test-suite/expr)
-
-;; Note: can rove find this?
-(deftest test-expr
-  (ok nil))
+;; 1. expr-realize
+;; 2. gflops test from the matmul schedule
+(deftest test-expr-simplification
+  ;; [TODO] More Simplification case if exists
+  (ok (= 1 (buffer-value (expr-realize (expr-add (expr-const 1 :int64) (expr-const 0 :int64)))))))
