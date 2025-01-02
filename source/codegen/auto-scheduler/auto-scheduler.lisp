44e12fbfa7ae17050910d3e3d3579d0195a3deee
hikettei <hikettei@gmail.com>
2025-01-02 21:08:17 +0900

    WIP

--- source/codegen/auto-scheduler/auto-scheduler.lisp
+++ source/codegen/auto-scheduler/auto-scheduler.lisp
@@ -23,39 +23,42 @@
   (caten/codegen/unroll:get-packable-band node-id (getattr si :polyhedral) #'caten/codegen/unroll:mark-unroll-p))
 
 (defclass OptPacking (Opt) nil) ;; horizontal unroll (used to float4 grouping, simd transformation, reduce, etc)
-;; (defclass OptInterchange (Opt) nil)
-;; (defclass OptGroup (Opt) nil) Implement decent GPU Support First.
-;; (defclass OptCollapse (Opt) nil) Effective for Tile+Paralle Fusion, but is it beyond the compiler?
+(defclass OptInterchange (Opt) nil)
+(defclass OptGrouptop (Opt) nil) ;; Implement decent GPU Support First.
 ;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ;; Purpose: get a list of optimal scheduling commands
 ;; [TODO] JIT_DEBUG >= 2 to see optimized schedule sequence by BEAM (TODO: Searching method like tiramisu)
 ;; ~~~ Schedule Templates ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-(defun gensketch (schedule-item &key (actions '(OptTile OptUnroll)) &aux (collected-sketchgen))
-  (loop for bp in (getattr schedule-item :blueprint)
-        if (eql (node-type bp) :EXPR) do
-          (loop for act in actions
-                for opt = (make-instance act :id (node-id bp))
-                when (opt-applicable-p opt (princ-to-string (node-id bp)) schedule-item)
-                  do (push opt collected-sketchgen)))
-  collected-sketchgen)
+;; [TODO] Parallelize == apply-tile 100あるループを4分割とかする
+;; CPU=local_size(1, 1, 1)
+;; GPU=local_size(32, 1, 1) global_size=(32, ...) -> gidx gidy gidz lidx lidy lid3
+;; ^ i.e.: parallelize = Tiling with size
+;; Implementation:
+;; - [ ] ISL Schedule Nodeに対してBEAM Searchを実施する
+;; - [ ] OPTIMIZE=2 will cache the optimized sequence of Opt
+;; - [ ] GPU: gid.x * local_size + lid.xこれをFirst Class Supportにする
+;; - [ ] (caten x :variables (list (variable 'b 0 10 2))) -> 全部に対してSampleする
+;; - [ ] Unroll/Tiling/Parallelizeを全部Tileで実装する
+;; - [ ] Loop Tilingした後にInterchangeができるようにScheduleする
+;; TODO: Cache the result from OPTIMIZE=2
+;; BEAM Search: ISL Schedule Treeで実施する
+(defun beam (schedule-node)
+  
+  )
 
 (defun auto-schedule (auto-scheduler node)
   (assert (getattr node :polyhedral))
   (symbol-macrolet ((OPTIMIZE (the (integer 0 2) (ctx:getenv :OPTIMIZE))))
     (when (= 0 OPTIMIZE) (return-from auto-schedule)) ;; No optimization
-    (let ((sketch (gensketch node)))
-      (print "OPTIMIZATION CANDIDATES")
-      (print sketch)
-      ;; generating sketch
-      (when (>= OPTIMIZE 2)
-        
-        )
+    ;; generating sketch
+    (when (>= OPTIMIZE 2)
       
-      (when (>= OPTIMIZE 1)
-        ;; Finally applies the outermost n loop parallelization for the interchanged loops.
-        (caten/codegen/coincidence:apply-parallel
-         (getattr node :polyhedral)
-         (auto-scheduler-n-global-loops auto-scheduler)))
-      ;; [TODO] Final BEAM Search for local/global size, or tiling size.
-      ;; Load blueprint from optimized polyhedral IR
-      (setf (getattr node :blueprint) (caten/codegen/ast-parser:lower-into-bp-from-polyhedral (caten/codegen/polyhedral:->ast (getattr node :polyhedral) (getattr node :rank)) node)))))
+      )
+    (when (>= OPTIMIZE 1)
+      ;; Finally applies the outermost n loop parallelization for the interchanged loops.
+      (caten/codegen/coincidence:apply-parallel
+       (getattr node :polyhedral)
+       (auto-scheduler-n-global-loops auto-scheduler)))
+    ;; [TODO] Final BEAM Search for local/global size, or tiling size.
+    ;; Load blueprint from optimized polyhedral IR
+    (setf (getattr node :blueprint) (caten/codegen/ast-parser:lower-into-bp-from-polyhedral (caten/codegen/polyhedral:->ast (getattr node :polyhedral) (getattr node :rank)) node))))
