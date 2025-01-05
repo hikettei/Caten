f128ba56ffc26fb5e88ee638944aad4acc01004b
atzmueller <martin.atzmueller@uni-osnabrueck.de>
2025-01-05 13:04:58 +0100

    minor edit/messages

--- source/apis/attrs.lisp
+++ source/apis/attrs.lisp
@@ -1,4 +1,4 @@
 (in-package :caten/apis)
 
-(defnode (:Special/VM :Pause/Backward) (JITAble) "During VM execution, the forward computation is paused at the point where this node exists."
+(defnode (:Special/VM :Pause/Backward) (JITAble) "During VM execution, forward computation is paused at the point where this node exists."
 	 :placeholder -1)
