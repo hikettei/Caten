8457c66553cb12f015fa8db6a734024a93e8b9da
hikettei <hikettei@gmail.com>
2025-01-07 20:07:00 +0900

    only compiles with __APPLE__

--- source/byoc/helpers/callback.lisp
+++ source/byoc/helpers/callback.lisp
@@ -2,12 +2,25 @@
 ;; Note2: create a closure that calls the stored callback (ensure it is thread-safe!)
 ;; Note3: this thing can be realized with only using cffi?
 (c
-"#include <stdio.h>
+"#ifdef __APPLE__
+#include <stdio.h>
+
 typedef void (*callback_t)(void);
 static callback_t stored_cb = NULL;
 
 void* closure_cffi_callback(void (*lisp_callback)(void)) {
-  stored_cb = lisp_callback;      
-  return (^{ stored_cb(); });
+    stored_cb = lisp_callback;
+    return (^{ stored_cb(); });
 }
+
+#else
+
+#include <stdio.h>
+
+void* closure_cffi_callback(void (*lisp_callback)(void)) {
+    fprintf(stderr, \"Error(caten/byoc/helper/callback.lisp): Apple Blocks extension not supported on this platform.\n\");
+    return NULL; 
+}
+
+#endif
 ")
