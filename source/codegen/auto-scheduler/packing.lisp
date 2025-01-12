c7ba02c408bb625ccca6c0cff01195d47314660f
hikettei <hikettei@gmail.com>
2025-01-12 18:15:04 +0900

    wip: vectorize

--- source/codegen/auto-scheduler/packing.lisp
+++ source/codegen/auto-scheduler/packing.lisp
@@ -8,28 +8,57 @@ TensorCore optimization is also implemented as a part of Vectorize.
   (:export
    #:Vectorize
    #:TensorCore
-   #:ast-rewrite-vectorize))
+   #:ast-rewrite-vectorize)
+  (:export
+   #:Vectorize-Config
+   #:Vectorize-Config-Vectorize))
 
 (in-package :caten/codegen/packing)
 
 (defstruct (Vectorize
-            (:constructor vectorize (dims &key (applicable-p #'identity) (rewriter nil))))
+            (:constructor vectorize (name dims &key (applicable-p #'identity) (rewriter nil))))
+  (name name :type keyword)
   (dims dims :type list)
   (applicable-p applicable-p :type function)
   (rewriter rewriter :type function))
 
-(defun apply-vectorize (vectorize)
+(defstruct Vectorize-Config
+  "applicable-p, rewriter will receive the necessary information via this structure to complete vectorize."
+  (vectorize (error "vectorize must occur") :type Vectorize))
 
-  )
+(defun apply-vectorize (user vectorize)
+  (declare (type user user))
+  (when (< (length (user-vectorize user)) (length (vectorize-dims vectorize)))
+    (return-from apply-vectorize nil))
+  (let ((padded
+          (append (loop repeat (- (length (user-vectorize user)) (length (vectorize-dims vectorize))) collect 1)
+                  (vectorize-dims vectorize))))
+    (assert (= (length padded) (length (user-vectorize user))))
+    (assert (every #'zerop (map 'list #'second (user-vectorize user))) () "apply-vectorze: range should start from zero.")
+    (let ((applicable-p (every #'(lambda (x y) (= 0 (mod x y))) (map 'list #'third (user-vectorize user)) padded))
+          (unroll (map 'list #'(lambda (x y) (/ x y)) (map 'list #'third (user-vectorize user)) padded))
+          (env (make-vectorize-config :vectorize vectorize)))
+      (when (and
+             applicable-p
+             ;; [TODO] Judge the elements are contiguous in the memory! (stride must be one if simd)
+             (funcall
+              (vectorize-applicable-p vectorize)
+              env))
+        (print unroll)
+        (print vectorize)
+        (print user)
+        (print (funcall (vectorize-rewriter vectorize) env))
+        ;; And finally return the vectorized ...
+        nil))))
 
-(defun TensorCore (dims)
+(defun TensorCore (dims &key (name :TensorCore))
   (declare (type list dims))
   (assert (and (= (length dims) 3) (every #'integerp dims)) () "TensorCore: dims are three-dimensional list of integers.")
-  (Vectorize dims :applicable-p #'identity :rewriter #'identity))
+  (Vectorize name dims :applicable-p #'identity :rewriter #'identity))
 ;; ~~ Vectorizer ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 (defun retrive-expr-with-id (id schedule-item)
   (declare (type string id) (type node schedule-item))
-  
+  nil
   )
 
 (defun ast-rewrite-vectorize (ast vectorize-rules schedule-item)
@@ -52,7 +81,7 @@ If some users are failed to be vectorized, they are rewritten as unroll."
                   (user-late-unroll-info ast) (copy-list (user-late-unroll-info ast)))
             (if (null (user-vectorize ast))
                 ast
-                (or (find-if #'(lambda (rule) (apply-vectorize rule)) vectorize-rules)
+                (or (find-if #'(lambda (rule) (apply-vectorize ast rule)) vectorize-rules)
                     (late-rewrite-pack->unroll ast)))))
          ;; Otherwise
          (AstBlock
--- source/test-suite/test-kernel-opt.lisp
+++ source/test-suite/test-kernel-opt.lisp
@@ -79,10 +79,11 @@
 
 (define-auto-scheduler
     (Mock-CPU-AutoScheduler ()) :n-global-loop 1
-    :vectorizes (list (Vectorize `(8) :rewriter #'identity)))
+    :vectorizes (list (Vectorize :gemm4x4 `(4 4) :rewriter #'identity)))
 
 (define-auto-scheduler (Mock-GPU-AutoScheduler ()) :n-global-loop 3)
 ;; To generate an optimized schedule for the cpu gemm kernel, we have to implement the following scheduling commands:
+;; [TODO] Interchange: only counts visible bands
 ;; - PARALLEL (OpenMP)
 ;; - Coleasing (Fuse PARALLEL Loop w/ tiled bands)
 ;; - Loop Tiling (2D)
