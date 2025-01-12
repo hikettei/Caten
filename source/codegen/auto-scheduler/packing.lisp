f0d7be07c503e66f81b344a0b9b0f0bdd47f0aa6
hikettei <hikettei@gmail.com>
2025-01-12 16:45:09 +0900

    first definitions

--- source/codegen/auto-scheduler/packing.lisp
+++ source/codegen/auto-scheduler/packing.lisp
@@ -1,3 +1,27 @@
 (defpackage :caten/codegen/packing
-  (:use :cl))
+  (:documentation "`caten/codegen/packing` provides a transformation tool to
+replace the packed region (defined in auto-scheduler.lisp, class Packing) with the vectorized ops.
+The class `Vectorize` will provide the information of the vectorized region, passed via define-auto-scheduler macro.
+TensorCore optimization is also implemented as a part of Vectorize.
+")
+  (:use :cl)
+  (:export
+   #:Vectorize
+   #:TensorCore))
+
 (in-package :caten/codegen/packing)
+
+(defstruct (Vectorize
+            (:constructor vectorize (dims &key (applicable-p #'identity) (rewriter nil))))
+  (dims dims :type list)
+  (applicable-p applicable-p :type function)
+  (rewriter rewriter :type function))
+
+(defun TensorCore (dims)
+  (declare (type list dims))
+  (assert (and (= (length dims) 3) (every #'integerp dims)) () "TensorCore: dims are three-dimensional list of integers.")
+  (Vectorize dims :applicable-p #'identity :rewriter #'identity))
+
+(defun apply-vectorize ()
+
+  )
