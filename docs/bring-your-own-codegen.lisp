26f122beef9ed37b8733d564f7a97f3bc897e5df
hikettei <hikettei@gmail.com>
2025-01-13 21:08:12 +0900

    docs

--- docs/bring-your-own-codegen.lisp
+++ docs/bring-your-own-codegen.lisp
@@ -6,3 +6,7 @@
 ;; Work in progress!
 ;; TODO: How to add a new accelerator to the codegen?
 ;; - intro-to-scheduler.lisp: Tutorial dedicated on optimizing gemm
+
+;; TODO:
+;; - caten/runtime
+;; - caten/codegen/renderer things
