9dbe8255c8960e8e4ba21d0996d6ecc98d937101
hikettei <hikettei@gmail.com>
2025-03-12 20:46:01 +0900

    fix: do not use type-relay in the renderer

--- source/codegen/renderer.lisp
+++ source/codegen/renderer.lisp
@@ -316,7 +316,7 @@
   (def :SQRT "sqrt"))
 
 (defmethod %render-node ((renderer CStyle-Renderer) (id (eql :RECIP)) node)
-  (let ((dtype (caten/runtime:buffer-dtype (car (relay-writes (read-type-relay node))))))
+  (let ((dtype (caten/ir:typed-dtype (car (getattr node :src-types)))))
     (if (caten/common.dtype:dtype/floatp dtype)
         (format nil "1.0/(~a)" (render-node renderer (nth 0 (node-reads node))))
         (format nil "1/(~a)" (render-node renderer (nth 0 (node-reads node)))))))
