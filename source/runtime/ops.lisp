91f5c007520352d8d0f1a598f2cb5d6b06aa50fe
hikettei <hikettei@gmail.com>
2025-07-01 17:43:36 +0900

    wip

--- source/runtime/ops.lisp
+++ source/runtime/ops.lisp
@@ -5,17 +5,32 @@
 
    ))
 (in-package :caten/runtime/ops)
+;; How about this? we move the definition and constructor of irs to:
+;; ./source/ir
+;; ./source/ir/tensor/
+;; ./source/ir/runtime/
+;; ./source/ir/codegen/
+
+;; Our Goal is turn every node as a compilable (e.g.: creating any runtime from our IR)
+(eval-when (:compile-toplevel :load-toplevel :execute)
+
+  (defnode (:Runtime :KERNEL_CALL) ()
+           "The node :KERNEL_CALL calls a compiled kernel."
+
+           )
+
+)
+
 ;; [TODO]
 ;; - Remove defnode in jit.lisp (e.g.: JIT_KERNEL), move to here!
-;; -
+;; - [Feat] TypeInferenceを統一的にしたい。
 ;; -
 
-(defun %fcall ()
-
+(defun %kernel_call ()
   )
 
-;; [TODO] Caten V2 TensorParallel
-(defun %shard ())
-(defun %gather())
+;; [TO COME] Caten V2 TensorParallel
+(defun %shard ()) ;; Breaks the tensor (todo: key axis)
+(defun %gather ())
 
 
