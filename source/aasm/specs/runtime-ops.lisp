3b88d1c4038215d8fc65988080d0c30596c8c2d6
hikettei <hikettei@gmail.com>
2025-07-05 20:22:07 +0900

    SINK

--- source/aasm/specs/runtime-ops.lisp
+++ source/aasm/specs/runtime-ops.lisp
@@ -1,1 +1,14 @@
 (in-package :caten/aasm)
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+
+(defclass RuntimeOps () nil)
+
+;; [TODO] Remove ./api/attrs.lisp after switching to use :SINK
+(defnode (:SPECIAL/VM :SINK) (JITAble RuntimeOps)
+	 "During VM execution, forward computation is paused at the point where this node exists."
+	 :placeholder -1
+         ;; :slots nil (TODO: Add :forward/:backward)
+         :type-relay #'(lambda (id->type node) (list (gethash (car (node-reads node)) id->type))))
+
+)
