737da44dfa83d3af87b7a1edd43c9130716b91d9
hikettei <hikettei@gmail.com>
2025-07-22 17:41:02 +0900

    wip

--- examples/flash-attention.lisp
+++ examples/flash-attention.lisp
@@ -14,19 +14,31 @@
 ;; Allow @caten.jit reader macro
 (in-caten-toplevel)
 
-;; TODO
+;; [TODO]
 ;; - 一時的なものなので，ちゃんとしたエラー検知，Specs, Docsを作る
+;; - Shape検査
 ;; - CStyle!
+;; - n-profile increase
+;; - !randを修正する ...
+;; - Implement Test (Written in Common Lisp)
 
 (progn
   @caten.jit () {
-  (defun Gemm ((Pointer X Type (M N)) (Pointer Y Type (N K)) (Pointer Z Type (M K)))
+  (defun Gemm ((Pointer Z Type (M K)) (Pointer X Type (M N)) (Pointer Y Type (N K)))
+    ;; [Note] Why nothing is scheduled?
     (for i = (Range 0 M) do
          (for j = (Range 0 K) do
               (with-locals ((acc 0.0))
                 (for k = (Range 0 N) do
                      (setf acc (+ acc (* (aref X (+ (* N i) k)) (aref Y (+ (* K k) j))))))
                 (setf (aref Z (+ (* K i) j)) acc)))))})
+
+(defun !matmul-jit (a b)
+  (let ((out (st "A[i j] B[j k] -> A[i k]" (a b))))
+    (Gemm out a b)))
+
+;; [TODO]
+;; !matmul-jit + Activation, etc, fusion
 
 (progn
   @caten.jit () {
