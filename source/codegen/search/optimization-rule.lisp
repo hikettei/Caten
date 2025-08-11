a712b017c43e23b21f195eaf0cd1d9982f8b77bd
hikettei <hikettei@gmail.com>
2025-08-11 15:16:32 +0900

    WORK

--- source/codegen/search/optimization-rule.lisp
+++ source/codegen/search/optimization-rule.lisp
@@ -104,8 +104,7 @@ options; typically used to seed candidate schedules at the start of search."))
            (compute-schedule-constraints
             (psi-domain poly)
             (psi-dependency-graph poly)))))
-    (setf (psi-theta poly) new-schedule)
-    (print poly)))
+    (setf (psi-theta poly) new-schedule)))
 ;; ~~ Interchange ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 (defclass Interchange (OptimizationRule)
   ((order :initarg :order :accessor interchange-order :type list)))
