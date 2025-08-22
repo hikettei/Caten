08c1a440d685e34b81efa5ac7dccc62c2f3f6e37
hikettei <hikettei@gmail.com>
2025-08-22 20:41:36 +0900

    Specs for scheduleops

--- source/aasm/specs/schedule-ops.lisp
+++ source/aasm/specs/schedule-ops.lisp
@@ -1,7 +1,28 @@
 (in-package :caten/aasm)
 
 (defclass ScheduleItem () nil)
+(defclass ScheduleTime (AType) ((type :initarg :type :reader st-type)))
 
-(defnode (:Schedule :Polyhedron) (ScheduleItem)
-    ""
-    :slots nil)
+(defun verify-schedule-item (self)
+  #'(lambda (id->type node)
+      (let ((ptypes (loop for i in (node-reads node) collect (gethash i id->type))))
+        (assert (every #'(lambda (x) (find (st-type x) `(:Affine :Nonaffine))) ptypes)))
+      (list (make-instance 'ScheduleTime :type self))))
+
+(defnode (:Schedule :Affine) (ScheduleItem)
+         "
+```
+  [VIEW]
+     ↓
+ [items x N] where each item is jitable
+     ↓
+   [out]
+```
+"
+         :slots nil
+         :type-relay (verify-schedule-item :Affine))
+
+(defnode (:Schedule :NonAffine) (ScheduleItem)
+         ""
+         :slots nil
+         :type-relay (verify-schedule-item :Nonaffine))
