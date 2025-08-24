d21a0603eca403f7bfa536d2020a4e4154e8be60
hikettei <hikettei@gmail.com>
2025-08-24 23:06:46 +0900

    New ISL Binding

---
+++ source/codegen/isl/caten.codegen.isl.asd
@@ -0,0 +1,17 @@
+(asdf:defsystem "caten.codegen.isl"
+  :description "ISL Binding and gc-extension for Codegen"
+  :author "hikettei <ichndm@gmail.com>"
+  :licence "MIT"
+  :defsystem-depends-on ("cffi-grovel")
+  :depends-on ("cffi" "trivial-garbage" "alexandria")
+  :serial t
+  :components
+  ((:file "package")
+   (:file "helpers")
+   (:cffi-grovel-file "grovel")
+   (:file "isl-binding")
+   (:file "isl-object")
+   (:file "isl-function")
+   (:file "object-specs")
+;;   (:file "specs/")
+   ))
---
+++ source/codegen/isl/isl-function.lisp
@@ -0,0 +1,51 @@
+(in-package :caten/codegen/isl)
+
+(defun parse-isl-args (args)
+  (loop for arg in args
+        for type = (car arg)
+        collect
+        (ecase type
+          (:keep `(:keep ,(second arg) ,(gensym (symbol-name (second arg)))))
+          (:parm arg)
+          (:take `(:keep ,(second arg) ,(gensym (symbol-name (second arg))))))))
+
+(defmacro define-isl-function (name primitive result &rest args)
+  (check-type name symbol)
+  (check-type primitive symbol)
+  (let ((args (parse-isl-args args)))
+    `(progn
+       (export ',name)
+       (declaim
+        (ftype
+         (function
+          (,@(loop for arg in args
+                   if (not (eql (car arg) :parm))
+                     collect (second arg)))
+          (values ,(second result)))
+         ,name))
+       (defun ,name (,@(loop for arg in args
+                             if (not (eql (car arg) :parm))
+                               collect (third arg)))
+         (declare
+          ,@(loop for arg in args
+                  if (not (eql (car arg) :parm))
+                    collect `(type ,@(cdr arg))))
+         (,(infer-result-wrapper (second result))
+          (,primitive
+           ,@(loop for arg in args
+                   for type = (car arg) for lisp-type = (second arg) for value = (third arg)
+                   collect
+                   (ecase type
+                     (:keep
+                      (if (isl-object-name-p lisp-type)
+                          `(isl-object-handle (__isl_keep ,value))
+                          value))
+                     (:parm
+                      (if (isl-object-name-p lisp-type)
+                          `(isl-object-handle (the ,lisp-type ,value))
+                          `(the ,lisp-type ,(third arg))))
+                     (:take
+                      (if (isl-object-name-p lisp-type)
+                          `(isl-object-handle (__isl_give ,value))
+                          value))
+                     (:null (error "Arguments with :null qualifier are not allowed."))))))))))
